9e4647d0aa7bcc2f27ee7380b50aeb810225949a
holyhi <holyhi@xm.com>
2020-06-09 19:58:25 +0800

    登录页面样式修改

--- src/config/router.config.js
+++ src/config/router.config.js
@@ -345,7 +345,7 @@ export const constantRouterMap = [
       {
         path: 'login',
         name: 'login',
-        component: () => import(/* webpackChunkName: "user" */ '@/views/user/Login')
+        component: () => import(/* webpackChunkName: "user" */ '@/views/user/LoginJet')
       },
       {
         path: 'register',
