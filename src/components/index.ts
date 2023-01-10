c2b83da156c2016e77165cf6982e270b7b8ec047
jackhoo_98 <jackhoo_98@foxmail.com>
2023-01-10 13:44:31 +0800

    feat: 新增TitleComponent组件

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,12 @@
 import type { App } from 'vue'
 import AIcon from './AIcon'
 import PermissionButton from './PermissionButton/index.vue'
+import TitleComponent from "./TitleComponent/index.vue";
 
 export default  {
     install(app: App) {
         app.component('AIcon', AIcon)
         app.component('PermissionButton', PermissionButton)
+        app.component('TitleComponent', TitleComponent)
     }
 }
