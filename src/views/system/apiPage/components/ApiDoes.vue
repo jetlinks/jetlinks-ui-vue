6728105539e3dd44e4f24e18851b5c076151462c
easy <1358086367@qq.com>
2023-01-12 17:39:02 +0800

    update: api配置

--- src/views/system/apiPage/components/ApiDoes.vue
+++ src/views/system/apiPage/components/ApiDoes.vue
@@ -8,22 +8,22 @@
             </div>
         </div>
 
-        <p style="display: flex; justify-content: space-between">
+        <p>
             <span class="label">请求数据类型</span>
-            <span class="value">{{
+            <span>{{
                 getContent(selectApi.requestBody) ||
                 'application/x-www-form-urlencoded'
             }}</span>
             <span class="label">响应数据类型</span>
-            <span class="value">{{ `["/"]` }}</span>
+            <span>{{ `["/"]` }}</span>
         </p>
 
         <div class="api-card">
             <h5>请求参数</h5>
             <div class="content">
                 <JTable
-                    :columns="columns.request"
-                    :dataSource="selectApi.parameters"
+                    :columns="requestCard.columns"
+                    :dataSource="requestCard.tableData"
                     noPagination
                     model="TABLE"
                 >
@@ -33,6 +33,39 @@
                     <template #type="slotProps">
                         <span>{{ slotProps.row.schema.type }}</span>
                     </template>
+                </JTable>
+            </div>
+        </div>
+        <div class="api-card">
+            <h5>响应状态</h5>
+            <div class="content">
+                <JTable
+                    :columns="responseStatusCard.columns"
+                    :dataSource="responseStatusCard.tableData"
+                    noPagination
+                    model="TABLE"
+                >
+                </JTable>
+
+                <a-tabs v-model:activeKey="responseStatusCard.activeKey">
+                    <a-tab-pane
+                        :key="key"
+                        :tab="key"
+                        v-for="key in tabs"
+                    ></a-tab-pane>
+                </a-tabs>
+            </div>
+        </div>
+
+        <div class="api-card">
+            <h5>响应参数</h5>
+            <div class="content">
+                <JTable
+                    :columns="respParamsCard.columns"
+                    :dataSource="respParamsCard.tableData"
+                    noPagination
+                    model="TABLE"
+                >
                 </JTable>
             </div>
         </div>
@@ -49,11 +82,21 @@ const props = defineProps({
         type: Object as PropType<apiDetailsType>,
         required: true,
     },
+    schemas: {
+        type: Object,
+        required: true,
+    },
 });
 const { selectApi } = toRefs(props);
 
-const columns = {
-    request: [
+type tableCardType = {
+    columns: object[];
+    tableData: object[];
+    activeKey?: any;
+    getData?: any;
+};
+const requestCard = reactive<tableCardType>({
+    columns: [
         {
             title: '参数名',
             dataIndex: 'name',
@@ -82,13 +125,138 @@ const columns = {
             scopedSlots: true,
         },
     ],
-};
+    tableData: [],
+    getData: () => {
+        requestCard.tableData = props.selectApi.parameters;
+    },
+});
+const responseStatusCard = reactive<tableCardType>({
+    activeKey: '',
+    columns: [
+        {
+            title: '状态码',
+            dataIndex: 'code',
+            key: 'code',
+        },
+        {
+            title: '说明',
+            dataIndex: 'desc',
+            key: 'desc',
+        },
+        {
+            title: 'schema',
+            dataIndex: 'schema',
+            key: 'schema',
+        },
+    ],
+    tableData: [],
+    getData: () => {
+        if (!Object.keys(props.selectApi.responses).length)
+            return (responseStatusCard.tableData = []);
 
-console.log(selectApi.value);
+        const tableData = <any>[];
+        Object.entries(props.selectApi.responses || {}).forEach((item: any) => {
+            const desc = item[1].description;
+            const schema = item[1].content['*/*'].schema.$ref?.split('/') || '';
+
+            tableData.push({
+                code: item[0],
+                desc,
+                schema: schema && schema.pop(),
+            });
+        });
+        responseStatusCard.activeKey = tableData[0]?.code;
+        responseStatusCard.tableData = tableData;
+    },
+});
+const tabs = computed(() =>
+    responseStatusCard.tableData
+        .map((item: any) => item.code + '')
+        .filter((code: string) => code !== '400'),
+);
+const respParamsCard = reactive<tableCardType>({
+    columns: [
+        {
+            title: '参数名称',
+            dataIndex: 'name',
+        },
+        {
+            title: '参数说明',
+            dataIndex: 'desc',
+        },
+        {
+            title: '类型',
+            dataIndex: 'type',
+        },
+    ],
+    tableData: [],
+    getData: (code: string) => {
+        type schemaObjType = {
+            paramsName: string;
+            paramsType: string;
+            desc: string;
+            children?: schemaObjType[];
+        };
+
+        const schemaName = responseStatusCard.tableData.find(
+            (item: any) => item.code === code,
+        ).schema;
+        console.log(1212, schemaName);
+        const schemas = toRaw(props.schemas);
+        function findData(schemaName: string) {
+            if (!schemaName || schemas[schemaName]) {
+                console.log(schemaName, schemas);
+                return [];
+            }
+            const result: schemaObjType[] = [];
+            const schema = schemas[schemaName];
+            const basicType = ['string', 'integer', 'boolean'];
+            Object.entries(schema.properties).forEach((item: [string, any]) => {
+                const paramsType =
+                    item[1].type ||
+                    (item[1].$ref && item[1].$ref.split('/').pop()) ||
+                    (item[1].items && item[1].items.$ref.split('/').pop()) ||
+                    '';
+                const schemaObj: schemaObjType = {
+                    paramsName: item[0],
+                    paramsType,
+                    desc: item[1].description || '',
+                };
+                if (!basicType.includes(paramsType))
+                    schemaObj.children = findData(paramsType);
+                result.push(schemaObj);
+            });
+            console.log(result);
+
+            return result;
+        }
+
+        respParamsCard.tableData = findData(schemaName);
+        // console.log(respParamsCard.tableData);
+    },
+});
+
 const getContent = (data: any) => {
-    if (!data) return '';
-    return Object.keys(data.content)[0];
+    if (data && data.content) {
+        return Object.keys(data.content || {})[0];
+    }
+    return '';
 };
+onMounted(() => {
+    requestCard.getData();
+    responseStatusCard.getData();
+});
+watch(
+    () => props.selectApi,
+    () => {
+        requestCard.getData();
+        responseStatusCard.getData();
+    },
+);
+
+watch([() => responseStatusCard.activeKey, () => props.selectApi], (n) => {
+    n[0] && respParamsCard.getData(n[0]);
+});
 </script>
 
 <style lang="less" scoped>
@@ -103,13 +271,25 @@ const getContent = (data: any) => {
 
         .input {
             display: flex;
+            margin: 24px 0;
         }
     }
+    p {
+        display: flex;
+        justify-content: space-between;
+        font-size: 14px;
 
+        .label {
+            font-weight: bold;
+        }
+    }
     .api-card {
+        margin-top: 24px;
         h5 {
             position: relative;
             padding-left: 10px;
+            font-weight: 600;
+            font-size: 16px;
 
             &::before {
                 position: absolute;
@@ -124,7 +304,6 @@ const getContent = (data: any) => {
         }
         .content {
             padding-left: 10px;
-
 
             :deep(.jtable-body) {
                 padding: 0;
