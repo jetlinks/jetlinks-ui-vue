c05cf5d961b74b50cc423eaffc6eb3a219a2c527
JiangQiming <291854119@qq.com>
2023-01-12 16:48:39 +0800

    feat: 通知配置详情表单

--- src/router/menu.ts
+++ src/router/menu.ts
@@ -48,6 +48,22 @@ export default [
         path: '/search',
         component: () => import('@/views/demo/Search.vue')
     },
+    {
+        path: '/notice/Config',
+        component: () => import('@/views/notice/Config/index.vue')
+    },
+    {
+        path: '/notice/Config/detail/:id',
+        component: () => import('@/views/notice/Config/Detail/index.vue')
+    },
+    {
+        path: '/notice/Template',
+        component: () => import('@/views/notice/Template/index.vue')
+    },
+    {
+        path: '/notice/Template/detail/:id',
+        component: () => import('@/views/notice/Template/Detail/index.vue')
+    },
     // end: 测试用, 可删除
 
     // link 运维管理
@@ -73,7 +89,7 @@ export default [
     },
     // 初始化
     {
-      path: '/init-home',
-      component: () => import('@/views/init-home/index.vue')
-  },
+        path: '/init-home',
+        component: () => import('@/views/init-home/index.vue')
+    },
 ]
