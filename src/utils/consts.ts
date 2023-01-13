0a2056bc0a78ffedd9fc493cc0571c9fc1cfc8b3
wangshuaiswim <shuai-1991@qq.com>
2023-01-13 09:45:52 +0800

    fix: 修改SystemConst定义

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -29,38 +29,21 @@ export const StatusColorEnum = {
   'default': 'default',
 }
 
-class SystemConst {
-  static API_BASE = 'api';
-
-  static SYSTEM_NAME = 'Jetlinks';
-
-  static LOGIN = 'LOGIN-STATUS';
-
-  static DOC_URL = 'http://doc.jetlinks.cn';
-
-  static BASE_CURD_MODAL_VISIBLE = 'BASE_CURD_MODAL_VISIBLE';
-
-  static BASE_CURD_CURRENT = 'BASE_CURD_CURRENT';
-
-  static BASE_CURD_MODEL = 'BASE_CURD_MODEL';
-
-  static BASE_UPDATE_DATA = 'BASE_UPDATE_DATA';
-
-  static GLOBAL_WEBSOCKET = 'GLOBAL-WEBSOCKET';
-
-  static BIND_USER_STATE = 'false';
-
-  static REFRESH_METADATA = 'refresh_metadata';
-
-  static REFRESH_METADATA_TABLE = 'refresh_metadata_table';
-
-  static GET_METADATA = 'get_metadata';
-
-  static REFRESH_DEVICE = 'refresh_device';
-
-  static AMAP_KEY = 'amap_key';
-
-  static Version_Code = 'version_code';
+export const SystemConst = {
+  API_BASE: 'api',
+  SYSTEM_NAME: 'Jetlinks',
+  LOGIN: 'LOGIN-STATUS',
+  DOC_URL: 'http://doc.jetlinks.cn',
+  BASE_CURD_MODAL_VISIBLE: 'BASE_CURD_MODAL_VISIBLE',
+  BASE_CURD_CURRENT: 'BASE_CURD_CURRENT',
+  BASE_CURD_MODEL: 'BASE_CURD_MODEL',
+  BASE_UPDATE_DATA: 'BASE_UPDATE_DATA',
+  GLOBAL_WEBSOCKET: 'GLOBAL-WEBSOCKET',
+  BIND_USER_STATE: 'false',
+  REFRESH_METADATA: 'refresh_metadata',
+  REFRESH_METADATA_TABLE: 'refresh_metadata_table',
+  GET_METADATA: 'get_metadata',
+  REFRESH_DEVICE: 'refresh_device',
+  AMAP_KEY: 'amap_key',
+  Version_Code: 'version_code',
 }
-
-export default SystemConst;
