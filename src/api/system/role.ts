12121c137682f6f6eff0aabe42503a5b08615fcb
easy <1358086367@qq.com>
2023-01-18 12:42:10 +0800

    update: 角色管理

--- src/api/system/role.ts
+++ src/api/system/role.ts
@@ -6,11 +6,15 @@ export const getRoleList_api = (data: any): Promise<any> => server.post(`/role/_
 export const delRole_api = (id: string): Promise<any> => server.remove(`/role/${id}`);
 // 保存角色
 export const saveRole_api = (data: any): Promise<any> => server.post(`/role`, data);
+// 获取角色详细信息
+export const getRoleDetails_api = (id: string): Promise<any> => server.get(`/role/${id}`);
 // 获取角色对应的权限树
 export const getPrimissTree_api = (id: string): Promise<any> => server.get(`/menu/role/${id}/_grant/tree`);
 
 
 // 获取用户列表
 export const getUserByRole_api = (data: any): Promise<any> => server.post(`/user/_query/`, data);
-// 将用户与该角色进行绑定
+// 将用户与角色绑定
 export const bindUser_api = (roleId:string, data: string[]): Promise<any> => server.post(`/role/${roleId}/users/_bind`, data);
+// 将用户与角色解绑
+export const unbindUser_api = (roleId:string, data: string[]): Promise<any> => server.post(`/role/${roleId}/users/_unbind`, data);
