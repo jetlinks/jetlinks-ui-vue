7cef68f828b5eaa1fce761d001956f1eba89d1f4
leiqiaochu <leiqiaochu@aossci.com>
2023-02-22 20:41:54 +0800

    feat: 告警中心告警配置

--- src/api/rule-engine/config.ts
+++ src/api/rule-engine/config.ts
@@ -1,5 +1,18 @@
 import server from '@/utils/request';
+import { LevelItem } from '@/views/rule-engine/Alarm/Config/typing';
 /**
  * 查询等级
  */
 export const queryLevel = () => server.get('/alarm/config/default/level');
+/**
+ * 保存告警等级
+ */
+export const saveLevel = (data:LevelItem[]) => server.patch('/alarm/config/default/level',data);
+/**
+ * 获取数据流转数据
+ */
+export const getDataExchange = (type:'consume' | 'producer') => server.get(`/alarm/config/${type}/data-exchange`);
+/**
+ * 保存告警数据输出
+ */
+export const saveOutputData =  (data:any) => server.patch('/alarm/config/data-exchange',data);
---
+++ src/views/rule-engine/Alarm/Config/Io/Save/output.vue
@@ -0,0 +1,109 @@
+<template>
+    <a-modal
+        :maskClosable="false"
+        width="45vw"
+        title="编辑"
+        @cancel="close"
+        @ok="save"
+        visible
+        cancelText="取消"
+        okText="确定"
+    >
+        <a-form layout="vertical" :model="outputData">
+            <a-form-item label="状态">
+                <a-switch
+                    checked-children="启用"
+                    un-checked-children="启用"
+                    v-model:checked="outputData.status"
+                ></a-switch>
+            </a-form-item>
+            <a-form-item
+                v-if="outputData.status"
+                label="kafka地址"
+                name="address"
+                :rules="[
+                    {
+                        required: true,
+                        message: '请输入kafka地址',
+                    },
+                    {
+                        max: 64,
+                        message: '最多输入64个字符',
+                    },
+                ]"
+            >
+                <a-input
+                    v-model:value="outputData.address"
+                    placeholder="请输入kafka地址"
+                ></a-input>
+            </a-form-item>
+            <a-form-item
+                v-if="outputData.status"
+                label="topic"
+                name="topic"
+                :rules="[
+                    {
+                        required: true,
+                        message: '请输入topic',
+                    },
+                    {
+                        max: 64,
+                        message: '最多输入64个字符',
+                    },
+                ]"
+            >
+                <a-input v-model:value="outputData.topic"></a-input>
+            </a-form-item>
+        </a-form>
+    </a-modal>
+</template>
+
+<script lang="ts" setup>
+import { Form } from 'ant-design-vue';
+import { saveOutputData } from '@/api/rule-engine/config';
+import { message } from 'ant-design-vue/es';
+const useForm = Form.useForm;
+const Myprops = defineProps({
+    data: {
+        default: '',
+    },
+});
+let outputData = reactive({
+    status: false,
+    address: '',
+    topic: '',
+});
+watchEffect(() => {
+    outputData.status =
+        Myprops.data?.data?.state?.value === 'enabled' ? true : false;
+    outputData.address = Myprops.data?.data?.config?.config?.address;
+    outputData.topic = Myprops.data?.data?.config?.config?.topic;
+});
+
+const close = () => {
+    emit('closeModel');
+};
+const save = () => {
+    saveOutputData({
+        config: {
+            sourceType: 'kafka',
+            config: {
+                ...outputData,
+                state: outputData?.status ? 'enabled' : 'disable',
+            },
+        },
+        state: outputData?.status ? 'enabled' : 'disable',
+        id: Myprops?.data?.data?.id,
+        sourceType: 'kafka',
+        exchangeType: 'producer',
+    }).then((res) => {
+        if (res.status === 200) {
+            message.success('操作成功');
+            emit('saveSuc');
+        }
+    });
+};
+const emit = defineEmits(['closeModel', 'saveSuc']);
+</script>
+<style lang="less" scoped>
+</style>
