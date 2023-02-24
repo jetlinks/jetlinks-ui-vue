86ee86d11926cc72bb5b89797f896ca1e0e288b8
easy <1358086367@qq.com>
2023-02-24 17:58:44 +0800

    update: 应用管理

--- src/api/system/apply.ts
+++ src/api/system/apply.ts
@@ -7,3 +7,8 @@ export const getApplyList_api = (data: any) => server.post(`/application/_query/
 export const changeApplyStatus_api = (id:string,data: any) => server.put(`/application/${id}`, data)
 // 删除应用
 export const delApply_api = (id:string) => server.remove(`/application/${id}`)
+
+
+
+// 获取组织列表
+export const getDepartmentList_api = () => server.get(`/organization/_all/tree`);
