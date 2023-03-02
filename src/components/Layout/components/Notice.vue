68a52db344885c61856e148adc8091420996749f
easy <1358086367@qq.com>
2023-03-02 21:40:21 +0800

    feat: 右上角部分

---
+++ src/components/Layout/components/Notice.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <a-dropdown :trigger="['click']">
+            <div style="height: 48px; display: flex; display: flex">
+                <AIcon type="BellOutlined" @click.prevent />
+            </div>
+            <template #overlay>
+                <div class="content">
+                    <NoticeInfo />
+                </div>
+            </template>
+        </a-dropdown>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { getList_api } from '@/api/account/notificationRecord';
+import NoticeInfo from './NoticeInfo.vue';
+
+const getList = () => {
+    const params = {
+        'terms[0].column': 'state',
+        'terms[0].value': 'unread',
+        'sorts[0].name': 'notifyTime',
+        'sorts[0].order': 'desc',
+    };
+    getList_api(params).then((resp) => {});
+};
+</script>
+
+<style lang="less" scoped></style>
--- src/views/account/NotificationRecord/components/ViewDialog.vue
+++ src/views/account/NotificationRecord/components/ViewDialog.vue
@@ -5,50 +5,55 @@
         width="1000px"
         @ok="emits('update:visible', false)"
         @cancel="emits('update:visible', false)"
+        class="view-dialog-container"
     >
         <a-row v-if="data?.targetType === 'device'">
-            <a-col :span="6" class="label">告警设备</a-col>
-            <a-col :span="6" class="value">
+            <a-col :span="4" class="label">告警设备</a-col>
+            <a-col :span="8" class="value">
                 {{ data?.targetName || '' }}
             </a-col>
-            <a-col :span="6" class="label">设备ID</a-col>
-            <a-col :span="6" class="value">
+            <a-col :span="4" class="label">设备ID</a-col>
+            <a-col :span="8" class="value">
                 {{ data?.targetId || '' }}
             </a-col>
         </a-row>
         <a-row>
-            <a-col :span="6" class="label">告警名称</a-col>
-            <a-col :span="6" class="value">
+            <a-col :span="4" class="label">告警名称</a-col>
+            <a-col :span="8" class="value">
                 {{ data?.alarmName || data?.alarmConfigName || '' }}
             </a-col>
-            <a-col :span="6" class="label">告警时间</a-col>
-            <a-col :span="6" class="value">
+            <a-col :span="4" class="label">告警时间</a-col>
+            <a-col :span="8" class="value">
                 {{ moment(data?.alarmTime).format('YYYY-MM-DD HH:mm:ss') }}
             </a-col>
 
-            <a-col :span="6" class="label">告警级别</a-col>
-            <a-col :span="6" class="value">
+            <a-col :span="4" class="label">告警级别</a-col>
+            <a-col :span="8" class="value">
                 {{ (levelList.length > 0 && getLevelLabel(data.level)) || '' }}
             </a-col>
-            <a-col :span="6" class="label">告警说明</a-col>
-            <a-col :span="6" class="value">{{ data?.description || '' }}</a-col>
+            <a-col :span="4" class="label">告警说明</a-col>
+            <a-col :span="8" class="value">{{ data?.description || '' }}</a-col>
 
-            <a-col :span="6" class="label">告警流水</a-col>
-            <a-col :span="18" class="value">
-                <!-- <MonacoEditor
-                    style="width: 100%; height: 370px"
-                    theme="vs"
-                    v-model="jsonData"
-                /> -->
-                <JsonViewer :value="jsonData" copyable boxed sort />
+            <a-col
+                :span="4"
+                class="label"
+                style="display: flex; height: 440px; align-items: center"
+                >告警流水</a-col
+            >
+            <a-col
+                :span="20"
+                class="value"
+                style="max-height: 440px; overflow: auto"
+            >
+                <JsonViewer :value="JSON.parse(data?.alarmInfo || '{}')" />
             </a-col>
         </a-row>
     </a-modal>
 </template>
 
 <script setup lang="ts">
-import MonacoEditor from '@/components/MonacoEditor/index.vue';
-import {JsonViewer} from 'vue3-json-viewer';
+import { JsonViewer } from 'vue3-json-viewer';
+import 'vue3-json-viewer/dist/index.css';
 import { queryLevel as queryLevel_api } from '@/api/rule-engine/config';
 import moment from 'moment';
 
@@ -69,18 +74,25 @@ const getLevel = () => {
     });
 };
 getLevel();
-const getLevelLabel = (id: string) => {
+const getLevelLabel = (id: number) => {
     if (levelList.value.length < 1 || !id) return '';
-    const obj = levelList.value.find((item) => item.id === id);
-    return obj.title;
+    const obj = levelList.value.find((item) => item.level === id);
+    return obj?.title;
 };
-
-const jsonData = JSON.stringify({
-    name: 'qiu', 
-    age: 18, 
-    isMan: false, 
-    arr: [1, 2, 5],
-});
 </script>
 
-<style scoped></style>
+<style lang="less" scoped>
+.view-dialog-container {
+    .ant-row {
+        .ant-col {
+            padding: 16px 24px;
+            border-right: 1px solid #f0f0f0;
+        }
+        .label {
+            background-color: #fafafa;
+        }
+        .value {
+        }
+    }
+}
+</style>
