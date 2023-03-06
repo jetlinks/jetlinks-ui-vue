7ed42f8acc8df44b47a2cf5e1ff0c5fdf4d19afe
JiangQiming <291854119@qq.com>
2023-03-06 11:29:56 +0800

    update: 搜索组件更换

--- src/views/media/Device/Channel/index.vue
+++ src/views/media/Device/Channel/index.vue
@@ -1,7 +1,7 @@
 <!-- 视频设备-通道列表 -->
 <template>
     <page-container>
-        <Search
+        <j-advanced-search
             type="simple"
             :columns="columns"
             target="product"
--- src/views/media/Device/index.vue
+++ src/views/media/Device/index.vue
@@ -1,6 +1,6 @@
 <template>
     <page-container>
-        <Search
+        <j-advanced-search
             :columns="columns"
             target="notice-config"
             @search="handleSearch"
