8622bfe8f1a12cf56f9513a2fc8f1715c1917cff
JiangQiming <291854119@qq.com>
2023-03-08 16:10:14 +0800

    update: 通知模板组件替换

--- src/views/notice/Template/Detail/components/ToTag.vue
+++ src/views/notice/Template/Detail/components/ToTag.vue
@@ -1,5 +1,5 @@
 <template>
-    <a-select
+    <j-select
         :options="options"
         @change="change"
         placeholder="请选择标签推送，多个标签用,号分隔"
--- src/views/notice/Template/Log/index.vue
+++ src/views/notice/Template/Log/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <a-modal v-model:visible="_vis" title="通知记录" :footer="null" width="70%">
+    <j-modal v-model:visible="_vis" title="通知记录" :footer="null" width="70%">
         <j-advanced-search
             type="simple"
             :columns="columns"
@@ -21,18 +21,18 @@
                 {{ moment(slotProps.notifyTime).format('YYYY-MM-DD HH:mm:ss') }}
             </template>
             <template #state="slotProps">
-                <a-space>
-                    <a-badge
+                <j-space>
+                    <j-badge
                         :status="slotProps.state.value"
                         :text="slotProps.state.text"
-                    ></a-badge>
+                    ></j-badge>
                     <AIcon
                         v-if="slotProps.state.value === 'error'"
                         type="ExclamationCircleOutlined"
                         style="color: #1d39c4; cursor: pointer"
                         @click="handleError(slotProps.errorStack)"
                     />
-                </a-space>
+                </j-space>
             </template>
             <template #action="slotProps">
                 <AIcon
@@ -42,7 +42,7 @@
                 />
             </template>
         </JProTable>
-    </a-modal>
+    </j-modal>
 </template>
 
 <script setup lang="ts">
