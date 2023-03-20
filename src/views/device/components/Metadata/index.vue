8cd2b8644ee6de2be37fde4800f6e6b559e20317
wangshuaiswim <shuai-1991@qq.com>
2023-03-20 21:14:41 +0800

    fix: 修改物模型BUG

--- src/views/device/components/Metadata/index.vue
+++ src/views/device/components/Metadata/index.vue
@@ -23,8 +23,8 @@
               :tooltip="{ title: '重置后将使用产品的物模型配置' }" key="reload">
               重置操作
             </PermissionButton>
-            <PermissionButton :hasPermission="`${permission}:update`" @click="visible = true">快速导入</PermissionButton>
-            <PermissionButton :hasPermission="`${permission}:update`" @click="cat = true">物模型TSL</PermissionButton>
+            <PermissionButton :hasPermission="`${permission}:update`" @click="visible = true" key="import">快速导入</PermissionButton>
+            <PermissionButton :hasPermission="`${permission}:update`" @click="cat = true" key="tsl">物模型TSL</PermissionButton>
           </j-space>
         </template>
 
@@ -54,9 +54,11 @@ import { useInstanceStore } from '@/store/instance'
 import Import from './Import/index.vue'
 import Cat from './Cat/index.vue'
 import BaseMetadata from './Base/index.vue'
+import { useMetadataStore } from '@/store/metadata'
 
 const route = useRoute()
 const instanceStore = useInstanceStore()
+const metadataStore = useMetadataStore()
 interface Props {
   type: 'product' | 'device';
   independentMetadata?: boolean;
@@ -73,7 +75,9 @@ const resetMetadata = async () => {
   const resp = await deleteMetadata(id as string)
   if (resp.status === 200) {
     message.info('操作成功')
-    instanceStore.refresh(id as string)
+    instanceStore.refresh(id as string).then(() => {
+      metadataStore.set('importMetadata', true)
+    })
     // Store.set(SystemConst.REFRESH_DEVICE, true)
     // setTimeout(() => {
     //   Store.set(SystemConst.REFRESH_METADATA_TABLE, true)
