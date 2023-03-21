7c42ebc587439f11502f4eb573c5463829f150b0
JiangQiming <291854119@qq.com>
2023-03-21 14:11:52 +0800

    fix: bug#10618、10608

--- src/views/notice/Template/Debug/index.vue
+++ src/views/notice/Template/Debug/index.vue
@@ -194,7 +194,7 @@ const getTemplateDetail = async () => {
                               },
                           },
                       ]
-                    : '',
+                    : [],
         }),
     );
 };
