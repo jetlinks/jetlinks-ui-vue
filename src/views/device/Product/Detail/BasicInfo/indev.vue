2ee291a7f0f598b774389e0e02de4844d02b01e7
leiqiaochu <leiqiaochu@aossci.com>
2023-03-28 14:05:13 +0800

    fix: bug#11094 11099 11107

--- src/views/device/Product/Detail/BasicInfo/indev.vue
+++ src/views/device/Product/Detail/BasicInfo/indev.vue
@@ -5,9 +5,16 @@
             <template #title>
                 <div style="display: flex">
                     <h3>配置信息</h3>
-                    <div style="margin: 0 0px 0 15px; color: #1d39c4">
-                        <AIcon type="EditOutlined" @click="editConfig"/>
-                    </div>
+                    <!-- <div style="margin: 0 0px 0 15px; color: #1d39c4">
+                        <AIcon type="EditOutlined" @click="editConfig" />
+                    </div> -->
+                    <PermissionButton
+                        type="link"
+                        @click="editConfig"
+                        hasPermission="device/Product:update"
+                    >
+                        <template #icon><AIcon type="EditOutlined" /></template>
+                    </PermissionButton>
                 </div>
             </template>
 
