57e107c5882b577fb627af5c329ebd9eb8165f81
xieyonghong <18010623010@163.com>
2023-03-30 16:03:30 +0800

    update: 新增FullPage组件

---
+++ src/components/Layout/FullPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class='full-page-warp' ref='fullPage' :style='{ minHeight: `calc(100vh - ${x + 24}px)`}'>
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang='ts' name='FullPage'>
+import { useElementBounding } from '@vueuse/core'
+
+const fullPage = ref(null)
+const { x } = useElementBounding(fullPage)
+
+</script>
+
+<style scoped>
+.full-page-warp {
+  background: #fff;
+}
+</style>
