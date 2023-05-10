4b401c3011ba6044cdd4b759e8202c356a1084fa
xieyonghong <18010623010@163.com>
2023-05-10 14:21:27 +0800

    fix: 优化license保存接口参数类型

--- src/api/system/license.ts
+++ src/api/system/license.ts
@@ -2,7 +2,11 @@ import server from '@/utils/request';
 
 export  const getModule = () => server.get('/license/module');
 
-export const licenseInit = (data:any) =>server.post('/license/init',data);
+export const licenseInit = (data:any) =>server.post('/license/init',data, {}, {
+  headers: {
+    'Content-Type': 'text/plain;charset=UTF-8'
+  }
+});
 
 export const getLicense = () => server.get('/license');
 
