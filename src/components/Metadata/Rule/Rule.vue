c44e319d9e0b5060f8f814806321d99384be0749
XieYongHong <18010623010@163.com>
2023-06-15 10:55:29 +0800

    feat: 添加读写类型插件

---
+++ src/components/Metadata/Rule/Rule.vue
@@ -0,0 +1,32 @@
+<template>
+  <j-form-item label="读写类型" required>
+
+  </j-form-item>
+  <j-form-item label="触发属性" required>
+
+  </j-form-item>
+  <j-form-item label="计算规则" required>
+    <div class="rule-add">
+      编辑规则
+    </div>
+  </j-form-item>
+  <j-form-item label="聚合函数" required>
+    <j-seltct
+      :options="[
+          { label: '无', value: null },
+          { label: '时间窗口', value: 'time' },
+          { label: '频次窗口', value: '' },
+      ]"
+    />
+  </j-form-item>
+</template>
+
+<script setup lang="ts" name="Rule">
+
+</script>
+
+<style scoped>
+.rule-add {
+
+}
+</style>
---
+++ src/components/Metadata/components/ReadType.vue
@@ -0,0 +1,18 @@
+<template>
+<j-form-item name="type" label="读写类型" required>
+  <j-select
+    :options="[
+        { label: '', value: '' }
+    ]"
+    placeholder="请选择读写类型"
+  />
+</j-form-item>
+</template>
+
+<script setup lang="ts" name="ReadType">
+
+</script>
+
+<style scoped>
+
+</style>
