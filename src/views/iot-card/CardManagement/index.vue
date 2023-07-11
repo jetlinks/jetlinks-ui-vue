f832e842db18b49fe8c8932acddb45966e064768
XieYongHong <18010623010@163.com>
2023-07-11 15:56:11 +0800

    fix: bug#11076

--- src/views/iot-card/CardManagement/index.vue
+++ src/views/iot-card/CardManagement/index.vue
@@ -756,7 +756,7 @@ const handleSearch = (e: any) => {
     const newParams = (e?.terms as any[])?.map(item1 => {
       item1.terms = item1.terms.map((item2: any) => {
         if (['cardStateType'].includes(item2.column) && !(['using', 'toBeActivated', 'deactivate'].includes(item2.value))) { // 处理其它状态
-          item2.termType = 'nin'
+          item2.termType = item2.termType === 'not' ? 'in' : 'nin'
         }
         return item2
       })
