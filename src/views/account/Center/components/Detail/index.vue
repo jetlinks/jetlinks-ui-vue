b0e83e7074e92de28c7ae7d831b79f1d42d60dcb
leiqiaochu <leiqiaochu@aossci.com>
2023-07-14 16:51:20 +0800

    fix: bug#16386

--- src/views/account/Center/components/Detail/index.vue
+++ src/views/account/Center/components/Detail/index.vue
@@ -3,7 +3,7 @@
         <div style="padding: 30px;">
             <div style="display: flex; padding-bottom: 24px; margin-bottom: 24px; border-bottom: 1px solid #E4E7F6">
                 <j-avatar :size="100" :src="userInfos.avatar"></j-avatar>
-                <div style="margin-left: 24px;">
+                <div style="margin-left: 24px;  max-width: 280px;" >
                     <div class="name"><j-ellipsis>{{ userInfos.name }}</j-ellipsis></div>
                     <div class="subTitle"><j-ellipsis>用户名: {{ userInfos?.username }}</j-ellipsis></div>
                     <!-- <div class="subTitle">账号ID: {{ userInfos?.id }}</div> -->
