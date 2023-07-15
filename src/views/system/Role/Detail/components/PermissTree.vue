6093e544b91ccbfc08ccce1d2d5664c3cbf02fbd
XieYongHong <18010623010@163.com>
2023-07-15 19:46:41 +0800

    fix: bug#16179

--- src/views/system/Role/Detail/components/PermissTree.vue
+++ src/views/system/Role/Detail/components/PermissTree.vue
@@ -48,6 +48,7 @@
             </template>
             <!-- 表格内容 -->
             <template #bodyCell="{ column, record }">
+                <div :id="record.id"></div>
                 <div v-if="column.key === 'menu'">
                     <j-checkbox
                         v-model:checked="record.granted"
@@ -110,6 +111,7 @@ import {
   USER_CENTER_MENU_CODE
 } from '@/utils/consts'
 import { isNoCommunity } from '@/utils/utils'
+import {useIndirectMenusMap} from "@/views/system/Role/Detail/components/util";
 
 const emits = defineEmits(['update:selectItems']);
 const route = useRoute();
@@ -270,6 +272,8 @@ const init = () => {
 };
 init();
 
+const { PermissionsMap } = useIndirectMenusMap(tableData)
+
 function getAllPermiss() {
     const id = route.params.id as string;
     getPrimissTree_api(id).then((resp) => {
@@ -297,6 +301,16 @@ function getAllPermiss() {
         }
     });
 }
+
+const hasIndirectMenus = (data: any) => {
+  if (data.children) {
+    const has = data.children.find(item => item.indirectMenus)
+    console.log(has)
+  } else if (data?.indirectMenus) {
+    console.log(data.indirectMenus)
+  }
+}
+
 /**
  * 菜单权限改变事件
  * @param row 触发的项
@@ -306,7 +320,9 @@ function menuChange(
     row: tableItemType,
     setButtonBool: boolean = true,
 ): undefined {
+  console.log('menuChange', row)
     // 判断是否需要对子菜单及操作权限进行选择
+  hasIndirectMenus(row)
     if (setButtonBool) {
         if (row.buttons && row.buttons.length > 0)
             row.buttons.forEach((button) => {
@@ -358,7 +374,7 @@ function menuChange(
     }
 
     emits('update:selectItems', selectList); // 选中的项传回父组件
-    treeRef.value.$forceUpdate();
+    proxy?.$forceUpdate?.();
 }
 
 /**
