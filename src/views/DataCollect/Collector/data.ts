1a668326c381840da334fb3e5c65f77b2b17c94a
XieYongHong <18010623010@163.com>
2023-07-24 11:23:28 +0800

    feat: task#2572

--- src/views/DataCollect/Collector/data.ts
+++ src/views/DataCollect/Collector/data.ts
@@ -1,7 +1,9 @@
 export const colorMap = new Map();
 colorMap.set('running', 'success');
 colorMap.set('partialError', 'warning');
+colorMap.set('partError', 'warning'); // 部分错误
 colorMap.set('failed', 'error');
+colorMap.set('allError', 'error'); // 全部错误
 colorMap.set('stopped', 'default');
 colorMap.set('processing', '#cccccc');
 colorMap.set('enabled', 'processing');
