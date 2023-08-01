daaf4bec20a51ad16a26c398c88d921c23a6583d
XieYongHong <18010623010@163.com>
2023-08-01 18:13:07 +0800

    fix: bug#16963

--- src/views/device/components/Metadata/Base/columns.tsx
+++ src/views/device/components/Metadata/Base/columns.tsx
@@ -168,7 +168,7 @@ export const useColumns = (type?: MetadataType, target?: 'device' | 'product', n
           { max: 64, message: '最多可输入64个字符' },
           {
             pattern: /^[a-zA-Z0-9_\-]+$/,
-            message: 'ID只能由数字、字母、下划线、中划线组成',
+            message: '标识只能由数字、字母、下划线、中划线组成',
           },
         ]
       },
