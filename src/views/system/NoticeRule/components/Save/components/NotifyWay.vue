8b2721e0bb2ca48a8b53e560a0792668aee86fdb
100011797 <2642441182@qq.com>
2023-08-01 18:15:59 +0800

    fix: 修改物模型导入bug

--- src/views/system/NoticeRule/components/Save/components/NotifyWay.vue
+++ src/views/system/NoticeRule/components/Save/components/NotifyWay.vue
@@ -69,9 +69,11 @@ onMounted(() => {
                     iconUrl: iconMap.get(item.id),
                 };
             });
-            emit('update:value', options.value?.[0]?.value);
-            emit('update:name', options.value?.[0]?.label);
-            emit('change', {label: options.value?.[0]?.label, value: options.value?.[0]?.value});
+            if(!props.value){
+                emit('update:value', options.value?.[0]?.value);
+                emit('update:name', options.value?.[0]?.label);
+                emit('change', {label: options.value?.[0]?.label, value: options.value?.[0]?.value});
+            }
         }
         loading.value = false;
     });
