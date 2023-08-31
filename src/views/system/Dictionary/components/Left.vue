97c4ace8a3a60a7ac9fb300361db99e4e3577a2f
leiqiaochu <leiqiaochu@aossci.com>
2023-08-31 15:42:30 +0800

    feat: 数据字典开头

---
+++ src/views/system/Dictionary/components/Left.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="left-contain">
+    <j-input placeholder="字典名称">
+        <template #suffix>
+            <AIcon type="SearchOutlined" />
+        </template>
+    </j-input>
+    <div class="controls">
+        <j-button type="primary">新增字典</j-button>
+        <j-button type="text">下载</j-button>
+        <j-button type="text">导入</j-button>
+    </div>
+    <div>
+        <j-list>
+            <template #renderItem="{ item }">
+                
+            </template>
+        </j-list>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+<style lang="less" scoped>
+.controls{
+    margin: 10px 0;
+}
+</style>
---
+++ src/views/system/Dictionary/index.vue
@@ -0,0 +1,21 @@
+<template>
+   <page-container>
+        <div class="dictionary_contain">
+            <div class="dictionary_left">
+                <Left/>
+            </div>
+            <div class="dictionary_right"></div>
+        </div>
+   </page-container>
+</template>
+
+<script lang="ts" setup>
+import Left from './components/Left.vue'
+</script>
+<style lang="less" scoped>
+.dictionary_contain{
+    display: flex;
+    background-color: #fff;
+    padding: 24px;
+}
+</style>
