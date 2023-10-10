dca46aeb43adf6ecdccfaefd2b01a9556472f71e
leiqiaochu <leiqiaochu@aossci.com>
2023-10-10 11:04:32 +0800

    feat: 关系组件

--- src/components/Relation/index.vue
+++ src/components/Relation/index.vue
@@ -1,10 +1,31 @@
 <template>
   <j-drawer :visible="true" title="关系绑定" placement="right" @close="closeDrawer" :width="500">
-    
+    <j-tabs v-model:activeKey="activeKey" type="card">
+      <j-tab-pane v-for="(item,index) in relationOptions" :key="item.value" :tab="item.text">
+        
+      </j-tab-pane>
+    </j-tabs>
   </j-drawer>
 </template>
 
 <script lang="ts" setup>
+const activeKey = ref('user')
+const relationOptions = [{
+  text:'用户',
+  value:'user'
+},{
+  text:'产品',
+  value:'product'
+},{
+  text:'设备',
+  value:'device'
+},{
+  text: '组织',
+  value:'organization'
+},{
+  text:'角色',
+  value:'role'
+}]
 const closeDrawer = () =>{
 
 }
