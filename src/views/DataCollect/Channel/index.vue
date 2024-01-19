ea81103df70d0f4b9609d2a2255442a4c236e743
FuHao <62583365+haodd0216@users.noreply.github.com>
2024-01-19 10:05:44 +0800

    fix: bug#21712、21711
    * fix: bug#21712、21711

--- src/views/DataCollect/Channel/index.vue
+++ src/views/DataCollect/Channel/index.vue
@@ -103,9 +103,11 @@
                                             <div class="card-item-content-text">
                                                 说明
                                             </div>
-                                            <div class="card-item-content-text">
-                                                <j-ellipsis>{{slotProps.description}}</j-ellipsis>
-                                            </div>
+                                            <j-ellipsis>
+                                              <div class="explain">
+                                                {{slotProps.description}}
+                                              </div>
+                                            </j-ellipsis>
                                         </j-col>
                                     </j-row>
                                 </div>
@@ -368,6 +370,9 @@ const handleSearch = (e: any) => {
         overflow: hidden; //超出的文本隐藏
         text-overflow: ellipsis; //溢出用省略号显示
         white-space: nowrap; //溢出不换行
+    }
+    .explain {
+      margin-top: 10px;
     }
 }
 .details-text {
