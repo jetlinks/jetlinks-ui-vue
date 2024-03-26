301d8868bf5a8e25489999ac005bd989828c2134
leiqiaochu <leiqiaochu@aossci.com>
2024-03-26 17:36:37 +0800

    feat: 默认ico

--- config/config.ts
+++ config/config.ts
@@ -2,7 +2,7 @@ export default {
   theme: {
     'primary-color': '#1d39c4',
   },
-  logo: '/favicon.ico', // 浏览器标签页logo
+  logo: '/TBEAIOT.ico', // 浏览器标签页logo
   title: '', // 浏览器标签页title
   layout: {
     title: '物联网平台', // 平台title
