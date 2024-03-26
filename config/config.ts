12c897a97f4ae349634ec65e445032d76f67e1ba
leiqiaochu <leiqiaochu@aossci.com>
2024-03-26 11:17:22 +0800

    feat: 修改网站title

--- config/config.ts
+++ config/config.ts
@@ -5,7 +5,7 @@ export default {
   logo: '/favicon.ico', // 浏览器标签页logo
   title: 'Jetlinks', // 浏览器标签页title
   layout: {
-    title: '特变电工', // 平台title
+    title: '', // 平台title
     logo: '/logo.png', // 平台logo
     mode: 'inline',
     theme: 'light', // 'dark' 'light'
