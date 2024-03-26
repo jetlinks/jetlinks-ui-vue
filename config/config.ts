f175abd5e92131e51d2ec4b77321d55e50138f5e
leiqiaochu <leiqiaochu@aossci.com>
2024-03-26 11:27:11 +0800

    feat: 删除系统默认标题

--- config/config.ts
+++ config/config.ts
@@ -3,9 +3,9 @@ export default {
     'primary-color': '#1d39c4',
   },
   logo: '/favicon.ico', // 浏览器标签页logo
-  title: 'Jetlinks', // 浏览器标签页title
+  title: '', // 浏览器标签页title
   layout: {
-    title: '', // 平台title
+    title: '物联网平台', // 平台title
     logo: '/logo.png', // 平台logo
     mode: 'inline',
     theme: 'light', // 'dark' 'light'
