0328f8f384227d86757c03c73d3783212b127b1d
leiqiaochu <leiqiaochu@aossci.com>
2024-04-01 15:59:00 +0800

    feat: 设备查询标签组件

--- src/views/device/Instance/components/TagSearch.vue
+++ src/views/device/Instance/components/TagSearch.vue
@@ -1,15 +1,29 @@
 <template>
-  <j-input @click="showTagSearch === true" v-model="searchValue">
+  <j-input @click="show" v-model="searchValue">
 
   </j-input>
-  <j-modal v-if="showTagSearch" title="设备标签" visible>
-    
+  <j-modal v-if="showTagSearch" title="筛选条件" visible>
+    <j-form layout="vertical">
+      <j-form-item label="设备标签:">
+        <j-row>
+          <j-col :span=12>
+            <j-input placeholder="请输入标签key"></j-input>
+          </j-col>
+          <j-col :span=12>
+            <j-input placeholder="请输入标签value"></j-input>
+          </j-col>
+        </j-row>
+      </j-form-item>
+    </j-form>
   </j-modal>
 </template>
 
 <script lang="ts" setup>
 const showTagSearch = ref(false)
 const searchValue = ref('')
+const show = () =>{
+   showTagSearch.value = true
+}
 </script>
 <style lang="less" scoped>
 </style>
