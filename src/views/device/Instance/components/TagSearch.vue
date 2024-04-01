3fc0bd5c815113b0afdf65a1e140b806dbbed8eb
leiqiaochu <leiqiaochu@aossci.com>
2024-04-01 15:06:34 +0800

    feat: 搜索设备标签

---
+++ src/views/device/Instance/components/TagSearch.vue
@@ -0,0 +1,15 @@
+<template>
+  <j-input @click="showTagSearch === true" v-model="searchValue">
+
+  </j-input>
+  <j-modal v-if="showTagSearch" title="设备标签" visible>
+    
+  </j-modal>
+</template>
+
+<script lang="ts" setup>
+const showTagSearch = ref(false)
+const searchValue = ref('')
+</script>
+<style lang="less" scoped>
+</style>
