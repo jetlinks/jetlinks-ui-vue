ffea17f8420d08ad2eb271c578780a3df10eb69f
leiqiaochu <leiqiaochu@aossci.com>
2024-04-11 17:14:47 +0800

    fix: bug#23519

--- src/views/iot-card/CardManagement/index.vue
+++ src/views/iot-card/CardManagement/index.vue
@@ -723,6 +723,10 @@ const getActions = (
                     const resp: any = await del(data.id);
                     if (resp.status === 200) {
                         onlyMessage('操作成功');
+                        const index = _selectedRowKeys.value.findIndex((id: any) => id === data.id);
+                        if (index !== -1) {
+                            _selectedRowKeys.value.splice(index, 1);
+                        }
                         cardManageRef.value?.reload();
                     } else {
                         onlyMessage('操作失败！', 'error');
