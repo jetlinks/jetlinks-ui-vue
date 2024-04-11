9fb277257a3a3e5e079b5c61aa9391aa9215b2e5
leiqiaochu <leiqiaochu@aossci.com>
2024-04-11 14:58:57 +0800

    fix: bug#23766、23759

--- src/views/system/Role/RoleLeft/index.vue
+++ src/views/system/Role/RoleLeft/index.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="left-contain">
-        <j-input placeholder="分组名称" v-model:value="searchValue" @pressEnter="search" @change="searchChange">
+        <j-input
+            placeholder="分组名称"
+            v-model:value="searchValue"
+            @pressEnter="search"
+            @change="searchChange"
+        >
             <template #suffix>
                 <AIcon type="SearchOutlined" @click="search" />
             </template>
@@ -11,157 +16,206 @@
             </j-button>
         </div>
         <div class="listBox">
-            <j-tree :tree-data="listData" v-if="listData.length" :fieldNames="{ title: 'name', key: 'id', children: 'children' }"
-                blockNode :selectedKeys="selectedKeys" :default-expanded-keys="['global_role']"
-                :showLine="{ showLeafIcon: false }">
+            <j-tree
+                :tree-data="listData"
+                v-if="listData.length"
+                :fieldNames="{ title: 'name', key: 'id', children: 'children' }"
+                blockNode
+                :selectedKeys="selectedKeys"
+                :default-expanded-keys="['global_role']"
+                :showLine="{ showLeafIcon: false }"
+            >
                 <template #title="item">
                     <div v-if="selectId === item.data.id">
-                        <j-input v-model:value="addName" @blur="() => saveGroup(item.data)" ref="inputRef"
-                            :maxlength="64"></j-input>
+                        <j-input
+                            v-model:value="addName"
+                            @blur="() => saveGroup(item.data)"
+                            ref="inputRef"
+                            :maxlength="64"
+                        ></j-input>
                     </div>
-                    <div class="treeItem" @click="() => selectGroup(item.data.id)" v-else>
+                    <div
+                        class="treeItem"
+                        @click="() => selectGroup(item.data.id)"
+                        v-else
+                    >
                         <template v-if="!item?.children">
                             <div class="itemText">
-                                <Ellipsis style="width: calc(100%-100px)">{{ item.name }}</Ellipsis>
+                                <Ellipsis style="width: calc(100%-100px)">{{
+                                    item.name
+                                }}</Ellipsis>
                             </div>
-                            <div @click="(e) => e.stopPropagation()" v-if="item.id !== 'default_group'">
-                                <PermissionButton type="text" hasPermission="system/Role:groupDelete" :popConfirm="{
-                                    title: `确定要删除？`,
-                                    onConfirm: () => deleteGroup(item.id),
-                                }" :disabled="item.id === 'default_group'">
+                            <div
+                                @click="(e) => e.stopPropagation()"
+                                v-if="item.id !== 'default_group'"
+                            >
+                                <PermissionButton
+                                    type="text"
+                                    hasPermission="system/Role:groupDelete"
+                                    :popConfirm="{
+                                        title: `确定要删除？`,
+                                        onConfirm: () => deleteGroup(item.id),
+                                    }"
+                                    :disabled="item.id === 'default_group'"
+                                >
                                     删除
                                 </PermissionButton>
-                                <PermissionButton type="text" hasPermission="system/Role:groupUpdate"
-                                    @click="editGroup(item.data)" :disabled="item.id === 'default_group'">
+                                <PermissionButton
+                                    type="text"
+                                    hasPermission="system/Role:groupUpdate"
+                                    @click="editGroup(item.data)"
+                                    :disabled="item.id === 'default_group'"
+                                >
                                     编辑
                                 </PermissionButton>
                             </div>
                         </template>
                         <template v-else>
-                            <Ellipsis style="width: calc(100%-100px)">{{ item.name }}</Ellipsis>
+                            <Ellipsis style="width: calc(100%-100px)">{{
+                                item.name
+                            }}</Ellipsis>
                         </template>
-                    </div> 
+                    </div>
                 </template>
             </j-tree>
-            <j-empty v-else style="margin-top: 100px;" />
+            <j-empty v-else style="margin-top: 100px" />
         </div>
     </div>
 </template>
-  
+
 <script lang="ts" setup>
 import { onlyMessage } from '@/utils/comm';
-import { queryRoleGroup, saveRoleGroup, deleteRoleGroup } from '@/api/system/role';
+import {
+    queryRoleGroup,
+    saveRoleGroup,
+    deleteRoleGroup,
+} from '@/api/system/role';
 import { randomString } from '@/utils/utils';
 import { useUserInfo } from '@/store/userInfo';
 import { storeToRefs } from 'pinia';
-const emit = defineEmits(['selectData'])
-const userInfoStore = useUserInfo()
-const { userInfos } = storeToRefs(userInfoStore)
+const emit = defineEmits(['selectData']);
+const userInfoStore = useUserInfo();
+const { userInfos } = storeToRefs(userInfoStore);
 const admin = computed(() => {
     return userInfos.value?.username === 'admin';
-})
-const listData: any = ref([{
-    name: '全局角色',
-    id: 'global_role',
-    children: []
-}])
-const selectedKeys = ref<string[]>(['global_role'])
-const searchValue = ref()
-const inputRef = ref()
-const addName = ref()   
-const selectId = ref()
+});
+const listData: any = ref([
+    {
+        name: '全局角色',
+        id: 'global_role',
+        children: [],
+    },
+]);
+const selectedKeys = ref<string[]>(['global_role']);
+const searchValue = ref();
+const inputRef = ref();
+const addName = ref();
+const selectId = ref();
 const queryGroup = async (select?: Boolean, searchName?: string) => {
-    const params = searchName ? { sorts: [{ name: 'createTime', order: 'desc' }], terms: [{ terms: [{ value: '%' + searchName + '%', termType: 'like', column: 'name' }] }] } : { sorts: [{ name: 'createTime', order: 'desc' }] }
-    const req: any = await queryRoleGroup(params)
+    const params = searchName
+        ? {
+              sorts: [{ name: 'createTime', order: 'desc' }],
+              terms: [
+                  {
+                      terms: [
+                          {
+                              value: '%' + searchName + '%',
+                              termType: 'like',
+                              column: 'name',
+                          },
+                      ],
+                  },
+              ],
+          }
+        : { sorts: [{ name: 'createTime', order: 'desc' }] };
+    const req: any = await queryRoleGroup(params);
     if (req.status === 200) {
-        listData.value[0].children = req.result
-        if(req.result[req.result.length - 1].id === 'default_group'){
-            req.result.unshift(req.result[req.result.length - 1])
-            req.result.pop()
+        listData.value[0].children = req.result;
+        if (req.result[req.result.length - 1].id === 'default_group') {
+            req.result.unshift(req.result[req.result.length - 1]);
+            req.result.pop();
         }
         // if(req.result.length && select){
         //     selectGroup(req.result[0].id)
         // }
     }
-}
+};
 const addGroup = () => {
-    addName.value = ''
-    const newId = randomString()
-    listData.value[0].children.splice(1, 0, ({
+    addName.value = '';
+    const newId = randomString();
+    listData.value[0].children.splice(1, 0, {
         name: '',
-        id: newId
-    }))
-    selectId.value = newId
+        id: newId,
+    });
+    selectId.value = newId;
     nextTick(() => {
-        inputRef.value.focus()
-    })
-}
+        inputRef.value.focus();
+    });
+};
 const saveGroup = async (data: any) => {
-    if (addName.value === '' && data.name==='' ) {
-        listData.value[0].children.splice(1, 1);
-    }
-    else {
-        if(addName.value === '' && data.name!==''){
-            addName.value=data.name
+    if (addName.value === '') {
+        if (data.name === '') {
+            listData.value[0].children.splice(1, 1);
         }
+    } else {
         const saveData = {
             name: addName.value,
-            id: data.id
-        }
-        const res = await saveRoleGroup(saveData)
+            id: data.id,
+        };
+        const res = await saveRoleGroup(saveData);
         if (res.status === 200) {
-            onlyMessage('操作成功!')
-            queryGroup()
+            onlyMessage('操作成功!');
+            queryGroup();
         } else {
-            onlyMessage('操作失败!')
+            onlyMessage('操作失败!');
         }
     }
-    setTimeout(()=>{
-        selectId.value = ''
-    },300)
-}
-
+    setTimeout(() => {
+        selectId.value = '';
+    }, 300);
+};
 
 const search = () => {
-    queryGroup(true, searchValue.value)
-}
+    queryGroup(true, searchValue.value);
+};
 const searchChange = () => {
     if (searchValue.value === '') {
-        queryGroup()
+        queryGroup();
     }
-}
-const selectGroup = (id: string) => {
-    selectedKeys.value = [id]
-    id === 'global_role' ? emit('selectData', '') : emit('selectData', selectedKeys.value)
-}
-const deleteGroup = async (id: string) => {
-    const res: any = await deleteRoleGroup(id)
-    if (res.status === 200) {
-        onlyMessage('操作成功!')
-        queryGroup(true)
-    } else {
-        onlyMessage('操作失败!')
-    }
-}
-const editGroup = (data: any) => {
-        if(!selectId.value){
-            selectId.value = data.id
-            addName.value = data.name
-            listData.value[0].children.forEach((item: any) => {
-                if (item.id === data.id) {
-                    item.edit = true
-                }
-            })
-            nextTick(() => {
-                inputRef.value.focus()
-            })
-        }
-        
 };
-       
+const selectGroup = (id: string) => {
+    selectedKeys.value = [id];
+    id === 'global_role'
+        ? emit('selectData', '')
+        : emit('selectData', selectedKeys.value);
+};
+const deleteGroup = async (id: string) => {
+    const res: any = await deleteRoleGroup(id);
+    if (res.status === 200) {
+        onlyMessage('操作成功!');
+        queryGroup(true);
+    } else {
+        onlyMessage('操作失败!');
+    }
+};
+const editGroup = (data: any) => {
+    if (!selectId.value) {
+        selectId.value = data.id;
+        addName.value = data.name;
+        listData.value[0].children.forEach((item: any) => {
+            if (item.id === data.id) {
+                item.edit = true;
+            }
+        });
+        nextTick(() => {
+            inputRef.value.focus();
+        });
+    }
+};
+
 onMounted(() => {
-    queryGroup(true)
-})
+    queryGroup(true);
+});
 </script>
 <style lang="less" scoped>
 .controls {
@@ -174,7 +228,7 @@ onMounted(() => {
 
     .itemText {
         line-height: 32px;
-        max-width: 40%
+        max-width: 40%;
     }
 }
 
