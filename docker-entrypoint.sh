acf2c006dba64b230d7f3796efd58df9aaffb948
XieYongHong <18010623010@163.com>
2024-04-30 15:48:19 +0800

    fix: 优化docker-entrypoint.sh替换为变量

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,12 +8,11 @@ if [ -z "$API_BASE_PATH" ]; then
     API_BASE_PATH="http://jetlinks:8844/";
 fi
 
-
 apiUrl="proxy_pass  $API_BASE_PATH\$1;"
 resolver="resolver $NAMESERVERS ipv6=off;"
 
 sed -i '11c '"$resolver"'' /etc/nginx/conf.d/default.conf
-sed -i '25c '"$apiUrl"'' /etc/nginx/conf.d/default.conf
+sed -i "s%{API_BASE_PATH}%$API_BASE_PATH%g" /etc/nginx/conf.d/default.conf
 
 if [ -z "$SERVER_NAME" ]; then
     serverName="server_name localhost;"
@@ -22,8 +21,10 @@ if [ -z "$SERVER_NAME" ]; then
 else
     serverName="server_name $SERVER_NAME;"
     sed -i '4c '"$serverName"'' /etc/nginx/conf.d/default.conf
-    sed -i 's/\${SERVER_NAME}/'$SERVER_NAME'/g' /etc/nginx/conf.d/default.conf
+    sed -i "s%{SERVER_NAME}%$SERVER_NAME%g" /etc/nginx/conf.d/default.conf
 fi
+
+#cat /etc/nginx/conf.d/default.conf
 
 nginx -g "daemon off;"
 
