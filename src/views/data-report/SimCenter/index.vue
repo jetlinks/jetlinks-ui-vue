a19bc589d310bbc42c73c0f5a7f550d2e4188e64
林虎 <lh9422324@gmail.com>
2024-05-23 18:16:25 +0800

    feat: 添加sim卡中心页

---
+++ src/views/data-report/SimCenter/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <page-container>
+        <full-page>
+            <div class="offline-statistics">simCenter</div>
+        </full-page>
+    </page-container>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.offline-statistics {
+}
+</style>
