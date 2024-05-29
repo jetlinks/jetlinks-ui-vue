c9df9405db55ce330fef462cde1749eec77c2eed
林虎 <lh9422324@gmail.com>
2024-05-29 14:21:24 +0800

    fix: 汽车中心调整

--- src/api/data-report/vehicleReport.ts
+++ src/api/data-report/vehicleReport.ts
@@ -5,16 +5,43 @@ import server from '@/utils/request';
  * @param data 查询条件
  */
 export const queryVehicleList = (data: any) =>
-    server.post('/vehicle/_query', data);
+    server.get('/vehicle/_query', data);
+
+/**
+ * 车辆列表导出
+ * @param format 格式
+ * @param data 数据
+ */
+export const vehicleExport = (format: string, data: any) =>
+    server.postStream(`/vehicle/_export/${format}`, data);
 
 /**
  * 车辆查询
  * @param id 查询条件
  */
 export const queryVehicleById = (id: any) => server.get(`/vehicle/${id}`);
+
 /**
- * 车辆查询
+ * 查询车辆关联设备
  * @param id 查询条件
  */
-export const queryVehicleEquipment = (data: any) =>
-    server.post(`/vehicle/rela/_query`, data);
+export const queryVehicleEquipmentList = (data?: any) =>
+    server.post<any>(`/vehicle/rela/device/_query`, data);
+/**
+ * 查询工作效率
+ * @param data 查询条件
+ */
+export const queryVehicleWorkList = (data?: any) =>
+    server.post(`/vehicle/es/work`, data);
+/**
+ * 查询工行驶记录
+ * @param data 查询条件
+ */
+export const queryVehicleTravelList = (data?: any) =>
+    server.post(`/vehicle/es/travel`, data);
+/**
+ * 查询在线离线表
+ * @param data 查询条件
+ */
+export const queryVehicleStatusList = (data?: any) =>
+    server.post(`/vehicle/es/status`, data);
