c0c37efc34c4eb86ee47eb815ebb9ceb57a87aa1
林虎 <lh9422324@gmail.com>
2024-05-30 16:37:53 +0800

    fix: 调整参数

--- src/views/data-report/IrregularDriving/components/OverSpeed.vue
+++ src/views/data-report/IrregularDriving/components/OverSpeed.vue
@@ -11,8 +11,8 @@
                 :columns="columns"
                 :request="querySpeed"
                 model="table"
-                defaultParams="{
-                    sorts: [{ name: 'createTime', order: 'desc' }],
+                :defaultParams="{
+                    sorts: [{ name: 'reportTime', order: 'desc' }],
                 }"
                 :params="params"
                 :gridColumn="3"
@@ -57,6 +57,9 @@
                     }"
                 >
                     {{ `${overSpeedInfoMaximumOverSpeed || 0}km/h` }}
+                </template>
+                <template #duration="{ duration }">
+                    {{ formatMillisecondsToHourMinute(duration) }}
                 </template>
             </JProTable>
         </full-page>
@@ -107,6 +110,33 @@ const handleExport = async () => {
         }
     });
 };
+
+const formatMillisecondsToHourMinute = (milliseconds: number) => {
+    const hours = Math.floor(milliseconds / 3600000);
+    const minutes = Math.floor((milliseconds % 3600000) / 60000);
+    if (
+        (typeof milliseconds === 'number' && isFinite(milliseconds)) &&
+        milliseconds > 0
+    ) {
+        return hours > 0
+            ? `${
+                  hours > 10
+                      ? hours.toString().padStart(2, '0')
+                      : hours.toString().padStart(1, '0')
+              }小时${
+                  minutes > 9
+                      ? minutes.toString().padStart(2, '0')
+                      : minutes.toString().padStart(1, '0')
+              }分`
+            : `${
+                  minutes > 9
+                      ? minutes.toString().padStart(2, '0')
+                      : minutes.toString().padStart(1, '0')
+              }分`;
+    } else {
+        return '0分';
+    }
+};
 const params = ref<Record<string, any>>({});
 const columns = [
     {
@@ -115,25 +145,25 @@ const columns = [
         key: 'vehicleTypeEnum',
         scopedSlots: true,
         search: {
-            type: 'select',
-            options: [
-                {
-                    label: '内燃柴油机',
-                    value: 'ICDieselEngine',
-                },
-                {
-                    label: '内燃汽油机',
-                    value: 'ICGasolineEngine',
-                },
-                {
-                    label: '机械柴油机',
-                    value: 'MachineDieselEngine',
-                },
-                {
-                    label: '内燃牵引车',
-                    value: 'ICTractor',
-                },
-            ],
+            type: 'string',
+            // options: [
+            //     {
+            //         label: '内燃柴油机',
+            //         value: 'ICDieselEngine',
+            //     },
+            //     {
+            //         label: '内燃汽油机',
+            //         value: 'ICGasolineEngine',
+            //     },
+            //     {
+            //         label: '机械柴油机',
+            //         value: 'MachineDieselEngine',
+            //     },
+            //     {
+            //         label: '内燃牵引车',
+            //         value: 'ICTractor',
+            //     },
+            // ],
         },
     },
     {
@@ -183,6 +213,7 @@ const columns = [
     {
         title: '持续时间 ',
         dataIndex: 'duration',
+        scopedSlots: true,
         key: 'duration',
         ellipsis: true,
         search: {
