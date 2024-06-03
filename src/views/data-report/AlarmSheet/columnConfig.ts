c7e6d8cb73dda97fe85c36ceaa246e20d358fcde
ZhongWenrui <1715526218@qq.com>
2024-06-03 09:50:42 +0800

    fix: 修复了车辆中心详情页idleDuration字段缺失页面不显示

---
+++ src/views/data-report/AlarmSheet/columnConfig.ts
@@ -0,0 +1,88 @@
+export const columns = [
+    {
+        title: '车辆类型',
+        dataIndex: 'vehicleTypeEnum',
+        key: 'vehicleTypeEnum',
+        ellipsis: true,
+        scopedSlots: true,
+        search: {
+            type: 'string',
+        },
+    },
+    {
+        title: '出厂编号',
+        dataIndex: 'factoryNumber',
+        key: 'factoryNumber',
+        ellipsis: true,
+        search: {
+            type: 'string',
+        },
+    },
+    {
+        title: '车辆简称',
+        dataIndex: 'simpleName',
+        key: 'simpleName',
+        ellipsis: true,
+        search: {
+            type: 'string',
+        },
+    },
+    {
+        title: '型号',
+        dataIndex: 'modelNumber',
+        key: 'modelNumber',
+        ellipsis: true,
+        search: {
+            type: 'string',
+        },
+    },
+    {
+        title: '所属组织',
+        dataIndex: 'orgName',
+        key: 'orgName',
+        scopedSlots: true,
+        search: {
+            type: 'string',
+        },
+    },
+    {
+        title: '告警时间',
+        dataIndex: 'alarmTime',
+        key: 'alarmTime',
+        width: 180,
+        scopedSlots: true,
+        search: {
+            type: 'date',
+        },
+    },
+    {
+        title: '车辆位置',
+        dataIndex: 'lngLat',
+        key: 'lngLat',
+        ellipsis: true,
+        scopedSlots: true,
+        search: {
+            type: 'string',
+        },
+    },
+    {
+        title: '告警设备',
+        dataIndex: 'deviceId',
+        key: 'deviceId',
+        scopedSlots: true,
+        ellipsis: true,
+        search: {
+            type: 'string',
+        },
+    },
+    {
+        title: '告警说明 ',
+        dataIndex: 'description',
+        key: 'description',
+        scopedSlots: true,
+        ellipsis: true,
+        search: {
+            type: 'string',
+        },
+    },
+];
--- src/views/data-report/AlarmSheet/index.vue
+++ src/views/data-report/AlarmSheet/index.vue
@@ -14,6 +14,7 @@
                 :params="params"
                 :gridColumn="3"
                 :row-selection="rowSelection"
+                :pagination="false"
             >
                 <template #headerTitle>
                     <j-space>
@@ -50,101 +51,13 @@ import { useFilterAlarmDesc } from '@/hook/useFilterAlarmDesc';
 import { FullPage } from 'components/Layout';
 import dayjs from 'dayjs';
 import { onlyMessage } from '@/utils/comm';
+import { columns } from './columnConfig';
 
 const configRef = ref<Record<string, any>>({});
 const params = ref<Record<string, any>>({});
 
 // 生成请求函数
 const queryData = useFilterAlarmDesc(queryAlarmData, 'alarmTime');
-
-const columns = [
-    {
-        title: '车辆类型',
-        dataIndex: 'vehicleTypeEnum',
-        key: 'vehicleTypeEnum',
-        ellipsis: true,
-        scopedSlots: true,
-        search: {
-            type: 'string',
-        },
-    },
-    {
-        title: '出厂编号',
-        dataIndex: 'factoryNumber',
-        key: 'factoryNumber',
-        ellipsis: true,
-        search: {
-            type: 'string',
-        },
-    },
-    {
-        title: '车辆简称',
-        dataIndex: 'simpleName',
-        key: 'simpleName',
-        ellipsis: true,
-        search: {
-            type: 'string',
-        },
-    },
-    {
-        title: '型号',
-        dataIndex: 'modelNumber',
-        key: 'modelNumber',
-        ellipsis: true,
-        search: {
-            type: 'string',
-        },
-    },
-    {
-        title: '所属组织',
-        dataIndex: 'orgName',
-        key: 'orgName',
-        scopedSlots: true,
-        search: {
-            type: 'string',
-        },
-    },
-    {
-        title: '告警时间',
-        dataIndex: 'alarmTime',
-        key: 'alarmTime',
-        width: 180,
-        scopedSlots: true,
-        search: {
-            type: 'date',
-        },
-    },
-    {
-        title: '车辆位置',
-        dataIndex: 'lngLat',
-        key: 'lngLat',
-        ellipsis: true,
-        scopedSlots: true,
-        search: {
-            type: 'string',
-        },
-    },
-    {
-        title: '告警设备',
-        dataIndex: 'deviceId',
-        key: 'deviceId',
-        scopedSlots: true,
-        ellipsis: true,
-        search: {
-            type: 'string',
-        },
-    },
-    {
-        title: '告警说明 ',
-        dataIndex: 'description',
-        key: 'description',
-        scopedSlots: true,
-        ellipsis: true,
-        search: {
-            type: 'string',
-        },
-    },
-];
 
 /**
  * 搜索
