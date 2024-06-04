2534e22f23aee27da3673a2ce0f658bbc723f9a6
林虎 <lh9422324@gmail.com>
2024-06-04 20:01:15 +0800

    fix: 设备中心分页调整

--- src/views/data-report/deviceReport/index.vue
+++ src/views/data-report/deviceReport/index.vue
@@ -1,14 +1,33 @@
 <template>
     <page-container>
-        <pro-search :columns="columns" target="notice-config" @search="handleSearch" />
+        <pro-search
+            :columns="columns"
+            target="notice-config"
+            @search="handleSearch"
+        />
         <FullPage>
-            <JProTable ref="configRef" :columns="columns" :request="request" model="table" :defaultParams="{
-                sorts: [{ name: 'createTime', order: 'desc' }],
-            }" :params="params" :row-selection="rowSelection">
+            <JProTable
+                ref="configRef"
+                :columns="columns"
+                :request="queryData"
+                model="table"
+                :defaultParams="{
+                    sorts: [{ name: 'createTime', order: 'desc' }],
+                }"
+                :params="globParams"
+                :row-selection="rowSelection"
+            >
                 <template #headerTitle>
                     <j-space>
-                        <j-popconfirm title="确认导出？" ok-text="确定" cancel-text="取消" @confirm="handleExport">
-                            <PermissionButton hasPermission="notice/Template:export">
+                        <j-popconfirm
+                            title="确认导出？"
+                            ok-text="确定"
+                            cancel-text="取消"
+                            @confirm="handleExport"
+                        >
+                            <PermissionButton
+                                hasPermission="notice/Template:export"
+                            >
                                 <AIcon type="ExportOutlined" />
                                 <span>导出</span>
                             </PermissionButton>
@@ -22,7 +41,9 @@
                     <span>{{ slotProps.describe || '--' }}</span>
                 </template>
                 <template #state="slotProps">
-                    <button v-if="slotProps.state.value === 'offline'" style="
+                    <button
+                        v-if="slotProps.state.value === 'offline'"
+                        style="
                             height: 24px;
                             padding: 3px 8px 3px 8px;
                             border-radius: 3px;
@@ -32,10 +53,13 @@
                             font-size: 14px;
                             font-weight: 400;
                             line-height: 18px;
-                        ">
+                        "
+                    >
                         离线
                     </button>
-                    <button v-else-if="slotProps.state.value === 'notActive'" style="
+                    <button
+                        v-else-if="slotProps.state.value === 'notActive'"
+                        style="
                             height: 24px;
                             padding: 3px 8px 3px 8px;
                             border-radius: 3px;
@@ -45,10 +69,13 @@
                             font-size: 14px;
                             font-weight: 400;
                             line-height: 18px;
-                        ">
+                        "
+                    >
                         禁用
                     </button>
-                    <button v-else style="
+                    <button
+                        v-else
+                        style="
                             height: 24px;
                             padding: 3px 8px 3px 8px;
                             border-radius: 3px;
@@ -58,9 +85,23 @@
                             font-size: 14px;
                             font-weight: 400;
                             line-height: 18px;
-                        ">
+                        "
+                    >
                         在线
                     </button>
+                </template>
+                <template #paginationRender>
+                    <a-pagination
+                        showQuickJumper
+                        isShowContent
+                        showSizeChanger
+                        :pageSize="pageSize"
+                        :pageSizeOptions="['12', '24', '48', '96']"
+                        :current="currentPage"
+                        :total="dataTotal"
+                        :show-total="() => `总共 ${dataTotal} 条`"
+                        @change="handleOnChange"
+                    />
                 </template>
             </JProTable>
         </FullPage>
@@ -71,13 +112,23 @@
 import { queryDeviceList, deviceExport } from '@/api/data-report/deviceReport';
 import { onlyMessage } from '@/utils/comm';
 
-import { downloadFileByUrl, downloadObject } from '@/utils/utils';
+import { downloadFileByUrl } from '@/utils/utils';
 import moment from 'moment';
 
 const configRef = ref<Record<string, any>>({});
-const params = ref<Record<string, any>>({});
+// 全局的搜索参数
+const globParams = ref<Record<string, any>>({});
+
+// 表格数据总数
+const dataTotal = ref<number>(0);
+// 表格当前属于多少页
+const currentPage = ref<number>(1);
+// 表格每页显示多少条数据
+const pageSize = ref<number>(12);
 
 const selectIds = ref<Array<number | string>>([]);
+
+const type = ref<string>('xlsx');
 
 const columns = [
     {
@@ -146,31 +197,77 @@ const columns = [
 ];
 
 /**
+ * @function handleOnChange 分页器改变的回调事件
+ * @param num
+ * @param pageSize
+ */
+const handleOnChange = (num: number, pageSize: number) => {
+    const _params = {
+        ...globParams,
+
+        // 因为分页器发生改变时会自动改变当前页码和每页条数
+        // 因此在这覆盖globSearchParam中的pageIndex和pageSize
+        pageIndex: num - 1,
+        pageSize: pageSize,
+    };
+    handleSearch(_params);
+};
+
+// 为了能够取到请求的条件，需要对请求再包装一层请求
+const queryData = async (_params: any) => {
+    const resp: any = await queryDeviceList(_params);
+    if (resp.status === 200) {
+        dataTotal.value = resp.result.total;
+        currentPage.value = resp.result.pageIndex + 1;
+        pageSize.value = resp.result.pageSize;
+        return {
+            // 3.仿造请求结果返回给表格
+            code: resp.status,
+            result: resp.result,
+            status: resp.status,
+        };
+    } else {
+        return {
+            code: 200,
+            result: { data: [] },
+            status: 200,
+        };
+    }
+};
+
+/**
  * 搜索
  * @param param
  */
 const handleSearch = (param: any) => {
-    params.value = param;
+    globParams.value = param;
 };
 
 /**
- * 导出
+ * @function handleExport 导出
  */
-const type = ref<string>('xlsx');
 const handleExport = async () => {
-    if (!selectIds.value?.length) {
-        onlyMessage('请勾选需要导出的数据', 'error');
-        return;
+    let _params: any = {};
+    if (selectIds.value?.length > 0) {
+        _params = {
+            terms: [
+                {
+                    column: 'id',
+                    value: selectIds.value,
+                    termType: 'in',
+                },
+            ],
+        };
+    } else {
+        // 当全不选时，为导出接口添加筛选条件
+        if (globParams.value.terms.length > 0) {
+            _params.terms = [globParams.value.terms[0]?.terms[0]];
+        } else {
+            _params.terms = [];
+        }
     }
-    const _params = {
-        terms: [
-            {
-                column: 'id',
-                value: selectIds.value,
-                termType: 'in',
-            },
-        ],
-    };
+
+    // 注意这里的请求函数要更换为当前页面的请求函数，以及下方导出的文件名
     deviceExport('设备消息数据', type.value, _params).then((res: any) => {
         if (res) {
             const blob = new Blob([res.data], { type: type.value });
@@ -185,6 +282,7 @@ const handleExport = async () => {
         }
     });
 };
+
 const rowSelection = {
     onChange: (selectedRowKeys: (string | number)[], selectedRows: any) => {
         console.log(
@@ -201,30 +299,6 @@ const rowSelection = {
         console.log(selected, selectedRows, changeRows);
     },
 };
-
-const request = (params: Record<string, any>) =>
-    new Promise((resolve) => {
-        queryDeviceList({
-            pageIndex: params.pageIndex,
-            pageSize: params.pageSize,
-            sorts: params.sorts,
-            terms: params.terms,
-        })
-            .then((response: any) => {
-                resolve({
-                    result: {
-                        data: response.result?.data,
-                        pageIndex: params.pageIndex || 0,
-                        pageSize: params.pageSize || 20,
-                        total: response.result?.total,
-                    },
-                    status: response.status,
-                });
-            })
-            .catch((error: any) => {
-                console.log(error);
-            });
-    });
 </script>
 
 <style lang="less" scoped></style>
