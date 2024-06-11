b70b39df619a8ce479dfeb782e551f30fc0be7c4
Bain <1571865676@qq.com>
2024-06-11 11:51:30 +0800

    fix: 切换大屏跳转按钮图标

--- src/components/Layout/BasicLayoutPage.vue
+++ src/components/Layout/BasicLayoutPage.vue
@@ -1,29 +1,45 @@
 <template>
-  <j-pro-layout v-bind="layoutConf" v-model:collapsed="basicLayout.collapsed" v-model:openKeys="basicLayout.openKeys"
-    :selectedKeys="basicLayout.selectedKeys" :breadcrumb="basicLayout.pure ? undefined : { routes: breadcrumbs }"
-    :headerHeight='basicLayout.pure ? 1 : layout.headerHeight' :pure="basicLayout.pure" @backClick='routerBack'>
-    <template #breadcrumbRender="slotProps">
-      <a v-if="slotProps.route.index !== 0 && !slotProps.route.isLast" @click='jump(slotProps.route)'>
-        {{ slotProps.route.breadcrumbName }}
-      </a>
-      <span v-else style='cursor: default'>{{ slotProps.route.breadcrumbName }}</span>
-    </template>
-    <template #rightContentRender>
-      <div class="right-content">
-        <j-tooltip title="大屏" @click="onReport">
-          <AIcon type="ProfileOutlined" style="font-size: 16px" />
-        </j-tooltip>
-        <AIcon type="QuestionCircleOutlined" @click="toDoc" style="font-size: 16px;margin-left: 24px;" />
-        <Notice style="margin: 0 24px" />
-        <UserInfo />
-      </div>
-    </template>
-    <slot>
-      <router-view v-slot="{ Component }">
-        <component :is="components || Component" />
-      </router-view>
-    </slot>
-  </j-pro-layout>
+    <j-pro-layout
+        v-bind="layoutConf"
+        v-model:collapsed="basicLayout.collapsed"
+        v-model:openKeys="basicLayout.openKeys"
+        :selectedKeys="basicLayout.selectedKeys"
+        :breadcrumb="basicLayout.pure ? undefined : { routes: breadcrumbs }"
+        :headerHeight="basicLayout.pure ? 1 : layout.headerHeight"
+        :pure="basicLayout.pure"
+        @backClick="routerBack"
+    >
+        <template #breadcrumbRender="slotProps">
+            <a
+                v-if="slotProps.route.index !== 0 && !slotProps.route.isLast"
+                @click="jump(slotProps.route)"
+            >
+                {{ slotProps.route.breadcrumbName }}
+            </a>
+            <span v-else style="cursor: default">{{
+                slotProps.route.breadcrumbName
+            }}</span>
+        </template>
+        <template #rightContentRender>
+            <div class="right-content">
+                <j-tooltip title="大屏" @click="onReport">
+                    <AIcon type="AreaChartOutlined" style="font-size: 16px" />
+                </j-tooltip>
+                <AIcon
+                    type="QuestionCircleOutlined"
+                    @click="toDoc"
+                    style="font-size: 16px; margin-left: 24px"
+                />
+                <Notice style="margin: 0 24px" />
+                <UserInfo />
+            </div>
+        </template>
+        <slot>
+            <router-view v-slot="{ Component }">
+                <component :is="components || Component" />
+            </router-view>
+        </slot>
+    </j-pro-layout>
 </template>
 
 <script setup lang="ts" name="BasicLayoutPage">
@@ -31,16 +47,16 @@ import UserInfo from './components/UserInfo.vue';
 import Notice from './components/Notice.vue';
 import DefaultSetting from '../../../config/config';
 import { useMenuStore } from '@/store/menu';
-import { AccountMenu } from '@/router/menu'
+import { AccountMenu } from '@/router/menu';
 import { useSystem } from '@/store/system';
 import { storeToRefs } from 'pinia';
-import { useSlots } from 'vue'
+import { useSlots } from 'vue';
 
 type StateType = {
-  collapsed: boolean;
-  openKeys: string[];
-  selectedKeys: string[];
-  pure: boolean;
+    collapsed: boolean;
+    openKeys: string[];
+    selectedKeys: string[];
+    pure: boolean;
 };
 
 const router = useRouter();
@@ -50,87 +66,89 @@ const menu = useMenuStore();
 
 const system = useSystem();
 const { configInfo, layout, basicLayout } = storeToRefs(system);
-const slots = useSlots()
+const slots = useSlots();
 const layoutConf = reactive({
-  theme: DefaultSetting.layout.theme,
-  siderWidth: layout.value.siderWidth,
-  logo: DefaultSetting.layout.logo,
-  title: DefaultSetting.layout.title,
-  menuData: menu.siderMenus,
-  // menuData: menu.siderMenus,
-  splitMenus: true,
+    theme: DefaultSetting.layout.theme,
+    siderWidth: layout.value.siderWidth,
+    logo: DefaultSetting.layout.logo,
+    title: DefaultSetting.layout.title,
+    menuData: menu.siderMenus,
+    // menuData: menu.siderMenus,
+    splitMenus: true,
 });
 
 //跳转大屏系统
 const onReport = async () => {
-  const report = '9100';
-  const reportUrl = configInfo.value.front?.reportPath
-  const openUrl = `${reportUrl}/#/login?id=id&report=${report}`;
-  setTimeout(() => {
-    window.open(openUrl, '_blank');
-  }, 300);
+    const report = '9100';
+    const reportUrl = configInfo.value.front?.reportPath;
+    const openUrl = `${reportUrl}/#/login?id=id&report=${report}`;
+    setTimeout(() => {
+        window.open(openUrl, '_blank');
+    }, 300);
 };
 
 watchEffect(() => {
-  layoutConf.theme = configInfo.value.front?.headerTheme || DefaultSetting.layout.theme;
-  layoutConf.title = configInfo.value.front?.title || DefaultSetting.layout.title;
-  layoutConf.logo = configInfo.value.front?.logo || DefaultSetting.layout.logo;
-})
+    layoutConf.theme =
+        configInfo.value.front?.headerTheme || DefaultSetting.layout.theme;
+    layoutConf.title =
+        configInfo.value.front?.title || DefaultSetting.layout.title;
+    layoutConf.logo =
+        configInfo.value.front?.logo || DefaultSetting.layout.logo;
+});
 
 const components = computed(() => {
-  const componentName = route.matched[route.matched.length - 1]?.components?.default?.name
-  if (componentName !== 'BasicLayoutPage') {
-    return route.matched[route.matched.length - 1]?.components?.default
-  }
-  return undefined
-})
+    const componentName =
+        route.matched[route.matched.length - 1]?.components?.default?.name;
+    if (componentName !== 'BasicLayoutPage') {
+        return route.matched[route.matched.length - 1]?.components?.default;
+    }
+    return undefined;
+});
 
 /**
  * 面包屑
  */
 const breadcrumbs = computed(() => {
-  const paths = router.currentRoute.value.matched
+    const paths = router.currentRoute.value.matched;
 
-  return paths.map((item, index) => {
-    return {
-      index,
-      isLast: index === (paths.length - 1),
-      path: item.path,
-      breadcrumbName: (item.meta as any).title || '',
-    }
-  })
-}
-);
+    return paths.map((item, index) => {
+        return {
+            index,
+            isLast: index === paths.length - 1,
+            path: item.path,
+            breadcrumbName: (item.meta as any).title || '',
+        };
+    });
+});
 
 const routerBack = () => {
-  router.go(-1)
-}
-
+    router.go(-1);
+};
 
 const jump = (item: any) => {
-  router.push(item.path)
-}
+    router.push(item.path);
+};
 
 watchEffect(() => {
-  if (router.currentRoute) {
-    const paths = router.currentRoute.value.matched
-    basicLayout.value.selectedKeys = paths.map(item => item.path)
-    basicLayout.value.openKeys = paths.map(item => item.path)
-    console.log(paths) //
-  }
+    if (router.currentRoute) {
+        const paths = router.currentRoute.value.matched;
+        basicLayout.value.selectedKeys = paths.map((item) => item.path);
+        basicLayout.value.openKeys = paths.map((item) => item.path);
+        console.log(paths); //
+    }
 
-  if (route.query?.layout === 'false') {
-    basicLayout.value.pure = true
-  }
-})
+    if (route.query?.layout === 'false') {
+        basicLayout.value.pure = true;
+    }
+});
 
 const toDoc = () => window.open('http://doc.v2.jetlinks.cn/');
 </script>
 
 <style scoped>
 .right-content {
-  margin-right: 24px;
-  display: flex;
-  align-items: center;
+    margin-right: 24px;
+    display: flex;
+    align-items: center;
 }
 </style>
