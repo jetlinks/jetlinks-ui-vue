fb60d3eed2d10bf46e715286aa0bcb9fe9b887f5
Bain <1571865676@qq.com>
2024-06-20 09:17:43 +0800

    feat: 添加车辆管理列表

---
+++ src/views/vehicle/vehicleManagement/typings.d.ts
@@ -0,0 +1,12 @@
+export interface ActionsType {
+    key: string;
+    text?: string;
+    disabled?: boolean;
+    permission?: boolean;
+    onClick?: (data: any) => void;
+    style?: CSSProperties;
+    tooltip?: TooltipProps;
+    popConfirm?: PopconfirmProps;
+    icon?: string;
+    children?: ActionsType[];
+}
