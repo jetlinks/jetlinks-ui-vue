47e1e04084dd77e67bd2f560c0c85d3f36bfebed
XieYongHong <18010623010@163.com>
2024-06-21 17:51:25 +0800

    fix: bug#27368修复告警配置查询条件

--- src/views/rule-engine/Alarm/Configuration/Save/Scene/Save/CardBox.vue
+++ src/views/rule-engine/Alarm/Configuration/Save/Scene/Save/CardBox.vue
@@ -427,7 +427,7 @@ const jumpView = () => {
       position: absolute;
       top: 0;
       left: 0;
-      z-index: 2;
+      z-index: 99;
       display: flex;
       justify-content: center;
       width: 100%;
