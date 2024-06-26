2b69f04f4e154bdfc3926742911847b317de0f55
ZhongWenrui <1715526218@qq.com>
2024-06-26 11:41:59 +0800

    update: 推送配置页面

--- src/views/system/Push/components/EditConfigModal.vue
+++ src/views/system/Push/components/EditConfigModal.vue
@@ -3,224 +3,224 @@
         visible
         :title="dialogTitle"
         :maskClosable="false"
-        width="675px"
+        width="30%"
         @ok="confirm"
         @cancel="emits('update:visible', false)"
         class="edit-dialog-container"
         :confirmLoading="loading"
         cancelText="取消"
         okText="确定"
+        style="min-width: 350px"
     >
         <j-form ref="formRef" :model="formData" layout="vertical">
-            <j-row :gutter="24">
-                <j-col :span="12">
-                    <j-form-item
-                        name="companyName"
-                        label="公司名称"
-                        :rules="[
-                            {
-                                max: 50,
-                                message: '公司名称长度不能超过50个字符',
-                                trigger: 'change',
-                            },
-                        ]"
-                    >
-                        <j-input
-                            v-model:value="formData.companyName"
-                            placeholder="请输入姓名"
-                            disabled
-                        />
-                    </j-form-item>
-                </j-col>
-                <j-col :span="12">
-                    <j-form-item
-                        name="deviceRuleCode"
-                        label="设备规则"
-                        :rules="[
-                            {
-                                required: true,
-                                message: '请输入设备规则',
-                                trigger: 'change',
-                            },
-                            {
-                                max: 50,
-                                message: '设备规则长度不能超过50个字符',
-                                trigger: 'change',
-                            },
-                        ]"
-                    >
-                        <j-input
-                            v-model:value="formData.deviceRuleCode"
-                            placeholder="请输入设备规则"
-                        />
-                    </j-form-item>
-                </j-col>
-            </j-row>
-            <j-row :gutter="24" v-if="form.isShow(['add'])">
-                <j-col :span="12">
-                    <j-form-item
-                        name="address"
-                        label="发送地址"
-                        :rules="[
-                            {
-                                required: true,
-                                message: '请输入发送地址',
-                                trigger: 'change',
-                            },
-                        ]"
-                    >
-                        <j-input
-                            v-model:value="formData.address"
-                            placeholder="请输入发送地址"
-                        />
-                    </j-form-item>
-                </j-col>
-                <j-col :span="12">
-                    <j-form-item
-                        name="encryptType"
-                        label="加密类型"
-                        :rules="[
-                            {
-                                required: true,
-                                message: '请选择加密方式',
-                                trigger: 'change',
-                            },
-                        ]"
-                    >
-                        <j-select
-                            v-model:value="formData.encryptType"
-                            placeholder="请选择加密类型"
-                            :options="
-                                encryptTypeMap.map((item) => {
-                                    return {
-                                        label: item.name,
-                                        value: item.value,
-                                    };
-                                })
-                            "
-                        >
-                            <template #placeholder>
-                                <span>请选择加密类型</span>
-                            </template>
-                        </j-select>
-                    </j-form-item>
-                </j-col>
-            </j-row>
-            <j-row :gutter="24">
-                <j-col :span="12">
-                    <j-form-item
-                        name="username"
-                        label="账号"
-                        :rules="[
-                            {
-                                required: true,
-                                message: '请输入账号',
-                                trigger: 'change',
-                            },
-                            {
-                                max: 50,
-                                message: '账号长度不能超过50个字符',
-                                trigger: 'change',
-                            },
-                        ]"
-                    >
-                        <j-input
-                            v-model:value="formData.username"
-                            placeholder="请输入账号"
-                        />
-                    </j-form-item>
-                </j-col>
-                <j-col :span="12">
-                    <j-form-item
-                        name="password"
-                        label="密码"
-                        :rules="[
-                            {
-                                required: true,
-                                message: '请输入密码',
-                                trigger: 'change',
-                            },
-                            {
-                                max: 50,
-                                message: '密码长度不能超过50个字符',
-                                trigger: 'change',
-                            },
-                        ]"
-                    >
-                        <j-input
-                            v-model:value="formData.password"
-                            placeholder="请输入密码"
-                        />
-                    </j-form-item>
-                </j-col>
-            </j-row>
-            <j-row :gutter="24">
-                <j-col :span="12">
-                    <j-form-item
-                        name="efficaciousStartTime"
-                        label="有效开始时间"
-                        :rules="[
-                            {
-                                required: true,
-                                message: '请选择有效开始时间',
-                                trigger: 'change',
-                            },
-                        ]"
-                    >
-                        <j-date-picker
-                            type="datetime"
-                            showTime
-                            v-model:value="formData.efficaciousStartTime"
-                            placeholder="请选择有效开始时间"
-                            style="width: 100%"
-                            value-format="x"
-                        />
-                    </j-form-item>
-                </j-col>
-                <j-col :span="12">
-                    <j-form-item
-                        name="efficaciousEndTime"
-                        label="有效结束时间"
-                        :rules="[
-                            {
-                                required: true,
-                                message: '请选择有效结束时间',
-                                trigger: 'change',
-                            },
-                        ]"
-                    >
-                        <j-date-picker
-                            type="datetime"
-                            showTime
-                            v-model:value="formData.efficaciousEndTime"
-                            placeholder="请选择有效结束时间"
-                            style="width: 100%"
-                            value-format="x"
-                        />
-                    </j-form-item>
-                </j-col>
-            </j-row>
-            <j-row>
-                <j-col :span="24">
-                    <j-form-item
-                        name="publicKey"
-                        label="公钥"
-                        :rules="[
-                            {
-                                required: true,
-                                message: '请输入公钥',
-                                trigger: 'change',
-                            },
-                        ]"
-                    >
-                        <j-textarea
-                            v-model:value="formData.publicKey"
-                            placeholder="请输入公钥"
-                            :auto-size="{ minRows: 3, maxRows: 10 }"
-                        />
-                    </j-form-item>
-                </j-col>
-            </j-row>
+            <j-form-item
+                name="deviceRuleCode"
+                label="设备规则"
+                :rules="[
+                    {
+                        required: true,
+                        message: '请输入设备规则',
+                        trigger: 'change',
+                    },
+                    {
+                        max: 50,
+                        message: '设备规则长度不能超过50个字符',
+                        trigger: 'change',
+                    },
+                ]"
+            >
+                <j-input
+                    v-model:value="formData.deviceRuleCode"
+                    placeholder="请输入设备规则"
+                />
+            </j-form-item>
+            <!--            <j-row :gutter="24">-->
+            <!--                <j-col :span="12">-->
+            <!--                    <j-form-item-->
+            <!--                        name="companyName"-->
+            <!--                        label="公司名称"-->
+            <!--                        :rules="[-->
+            <!--                            {-->
+            <!--                                max: 50,-->
+            <!--                                message: '公司名称长度不能超过50个字符',-->
+            <!--                                trigger: 'change',-->
+            <!--                            },-->
+            <!--                        ]"-->
+            <!--                    >-->
+            <!--                        <j-input-->
+            <!--                            v-model:value="formData.companyName"-->
+            <!--                            placeholder="请输入姓名"-->
+            <!--                            disabled-->
+            <!--                        />-->
+            <!--                    </j-form-item>-->
+            <!--                </j-col>-->
+            <!--                <j-col :span="12">-->
+            <!--                    -->
+            <!--                </j-col>-->
+            <!--            </j-row>-->
+            <j-form-item
+                v-if="form.isShow(['add'])"
+                name="address"
+                label="发送地址"
+                :rules="[
+                    {
+                        required: true,
+                        message: '请输入发送地址',
+                        trigger: 'change',
+                    },
+                ]"
+            >
+                <j-input
+                    v-model:value="formData.address"
+                    placeholder="请输入发送地址"
+                />
+            </j-form-item>
+            <j-form-item
+                v-if="form.isShow(['add'])"
+                name="encryptType"
+                label="加密类型"
+                :rules="[
+                    {
+                        required: true,
+                        message: '请选择加密方式',
+                        trigger: 'change',
+                    },
+                ]"
+            >
+                <j-select
+                    v-model:value="formData.encryptType"
+                    placeholder="请选择加密类型"
+                    :options="
+                        encryptTypeMap.map((item) => {
+                            return {
+                                label: item.name,
+                                value: item.value,
+                            };
+                        })
+                    "
+                >
+                    <template #placeholder>
+                        <span>请选择加密类型</span>
+                    </template>
+                </j-select>
+            </j-form-item>
+            <!--            <j-row :gutter="24" >-->
+            <!--                <j-col :span="12">-->
+
+            <!--                </j-col>-->
+            <!--                <j-col :span="12">-->
+            <!--                    -->
+            <!--                </j-col>-->
+            <!--            </j-row>-->
+            <j-form-item
+                name="username"
+                label="账号"
+                :rules="[
+                    {
+                        required: true,
+                        message: '请输入账号',
+                        trigger: 'change',
+                    },
+                    {
+                        max: 50,
+                        message: '账号长度不能超过50个字符',
+                        trigger: 'change',
+                    },
+                ]"
+            >
+                <j-input
+                    v-model:value="formData.username"
+                    placeholder="请输入账号"
+                />
+            </j-form-item>
+            <j-form-item
+                name="password"
+                label="密码"
+                :rules="[
+                    {
+                        required: true,
+                        message: '请输入密码',
+                        trigger: 'change',
+                    },
+                    {
+                        max: 50,
+                        message: '密码长度不能超过50个字符',
+                        trigger: 'change',
+                    },
+                ]"
+            >
+                <j-input
+                    v-model:value="formData.password"
+                    placeholder="请输入密码"
+                />
+            </j-form-item>
+            <!--            <j-row :gutter="24">-->
+            <!--                <j-col :span="12">-->
+            <!--                    <j-form-item-->
+            <!--                        name="efficaciousStartTime"-->
+            <!--                        label="有效开始时间"-->
+            <!--                        :rules="[-->
+            <!--                            {-->
+            <!--                                required: true,-->
+            <!--                                message: '请选择有效开始时间',-->
+            <!--                                trigger: 'change',-->
+            <!--                            },-->
+            <!--                        ]"-->
+            <!--                    >-->
+            <!--                        <j-date-picker-->
+            <!--                            type="datetime"-->
+            <!--                            showTime-->
+            <!--                            v-model:value="formData.efficaciousStartTime"-->
+            <!--                            placeholder="请选择有效开始时间"-->
+            <!--                            style="width: 100%"-->
+            <!--                            value-format="x"-->
+            <!--                        />-->
+            <!--                    </j-form-item>-->
+            <!--                </j-col>-->
+            <!--                <j-col :span="12">-->
+            <!--                    <j-form-item-->
+            <!--                        name="efficaciousEndTime"-->
+            <!--                        label="有效结束时间"-->
+            <!--                        :rules="[-->
+            <!--                            {-->
+            <!--                                required: true,-->
+            <!--                                message: '请选择有效结束时间',-->
+            <!--                                trigger: 'change',-->
+            <!--                            },-->
+            <!--                        ]"-->
+            <!--                    >-->
+            <!--                        <j-date-picker-->
+            <!--                            type="datetime"-->
+            <!--                            showTime-->
+            <!--                            v-model:value="formData.efficaciousEndTime"-->
+            <!--                            placeholder="请选择有效结束时间"-->
+            <!--                            style="width: 100%"-->
+            <!--                            value-format="x"-->
+            <!--                        />-->
+            <!--                    </j-form-item>-->
+            <!--                </j-col>-->
+            <!--            </j-row>-->
+            <!--            <j-row>-->
+            <!--                <j-col :span="24">-->
+            <!--                    <j-form-item-->
+            <!--                        name="publicKey"-->
+            <!--                        label="公钥"-->
+            <!--                        :rules="[-->
+            <!--                            {-->
+            <!--                                required: true,-->
+            <!--                                message: '请输入公钥',-->
+            <!--                                trigger: 'change',-->
+            <!--                            },-->
+            <!--                        ]"-->
+            <!--                    >-->
+            <!--                        <j-textarea-->
+            <!--                            v-model:value="formData.publicKey"-->
+            <!--                            placeholder="请输入公钥"-->
+            <!--                            :auto-size="{ minRows: 3, maxRows: 10 }"-->
+            <!--                        />-->
+            <!--                    </j-form-item>-->
+            <!--                </j-col>-->
+            <!--            </j-row>-->
         </j-form>
     </j-modal>
 </template>
