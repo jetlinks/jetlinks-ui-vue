25119972885d5f20327c1f07ba08d72594e604c4
Bain <1571865676@qq.com>
2024-07-17 16:54:52 +0800

    feat: 添加车辆地图

---
+++ src/views/vehicle/LocationMonitoring/VehicleMap/index.vue
@@ -0,0 +1,254 @@
+<template>
+    <page-container>
+        <full-page fixed>
+            <div class="region-map">
+                <AMapComponent @init="initMap"> </AMapComponent>
+                <div class="tree-card">
+                    <MapTree @select-data="selectData" />
+                </div>
+                <div class="search-card">
+                    <j-input
+                        placeholder="请输入车辆简称"
+                        v-model:value="mapSearchValue"
+                        @pressEnter="mapSearch"
+                        style="height: 48px"
+                        @change="mapSearchChange"
+                    >
+                        <template #suffix>
+                            <AIcon type="SearchOutlined" @click="mapSearch" />
+                        </template>
+                    </j-input>
+                </div>
+                <div class="vehicleType-card">
+                    <j-radio-group
+                        class="radio-group"
+                        v-model:value="typeValue"
+                    >
+                        <j-radio class="type-radio radio-all" value="a"
+                            >全部车辆</j-radio
+                        >
+                        <j-radio class="type-radio radio-Alarm" value="b"
+                            >故障车辆</j-radio
+                        >
+                        <j-radio class="type-radio radio-Fault" value="c"
+                            >告警车辆</j-radio
+                        >
+                    </j-radio-group>
+                </div>
+            </div>
+        </full-page>
+    </page-container>
+</template>
+
+<script lang="ts" setup>
+import AMapComponent from '@/components/AMapComponent/index.vue';
+import MapTree from './mapTree/index.vue';
+const MapRef = ref();
+const loading = ref(true);
+const mapSearchValue = ref();
+const typeValue = ref('a');
+
+const groupId = ref();
+const selectData = (data: any) => {
+    groupId.value = data[0];
+    console.log('data', data);
+};
+
+const lineArr = [
+    [116.478935, 39.997761],
+    [116.479282, 39.99856],
+    [116.479658, 39.998528],
+    [116.481149, 39.998184],
+    [116.483633, 39.998935],
+    [116.484648, 39.999861],
+    [116.485401, 40.0000123],
+];
+
+const path = ref(lineArr);
+
+const initMap = (e: any) => {
+    // console.log('map', e);
+    loading.value = true;
+    MapRef.value = e;
+    loading.value = false;
+    const map = MapRef.value;
+    map.setZoom(12);
+    MapRef.value.setCenter(path.value[0], true);
+    const labelsLayer = new AMap.LabelsLayer({
+        zooms: [3, 20],
+        zIndex: 1000,
+        collision: true, //该层内标注是否避让
+        allowCollision: true, //不同标注层之间是否避让
+    });
+
+    //设置一个图标对象
+    const icon = {
+        type: 'image', //图标类型，现阶段只支持 image 类型
+        image: 'https://a.amap.com/jsapi_demos/static/demo-center/marker/express2.png', //可访问的图片 URL
+        size: [48, 21], //图片尺寸
+        anchor: 'center', //图片相对 position 的锚点，默认为 bottom-center
+    };
+
+    const text = {
+        content: '测试地点1', //要展示的文字内容
+        direction: 'right', //文字方向，有 icon 时为围绕文字的方向，没有 icon 时，则为相对 position 的位置
+        offset: [-20, -4], //在 direction 基础上的偏移量
+        //文字样式
+        style: {
+            fontSize: 12, //字体大小
+            fillColor: '#22886f', //字体颜色
+            strokeColor: '#fff', //描边颜色
+            strokeWidth: 2, //描边宽度
+        },
+    };
+
+    const labelMarker1 = new AMap.LabelMarker({
+        name: '标准1', //此属性非绘制文字内容，仅为标识使用
+        position: lineArr[0],
+        zIndex: 16,
+        rank: 1, //避让优先级
+        icon: icon, //标注图标，将 icon 对象传给 icon 属性
+        text: text, //标注文本，将 text 对象传给 text 属性
+    });
+
+    labelsLayer.add(labelMarker1);
+    map.add(labelsLayer);
+    labelMarker1.on('click', (e: any) => {
+        console.log('eee', e.data.data.position);
+        openInfo(e.data.data.position);
+    });
+    map.setFitView();
+};
+
+const openInfo = (data: any) => {
+    //构建信息窗体中显示的内容
+    var info = [];
+    info.push(
+        '<div><div><img style="float:left;" src=" https://webapi.amap.com/images/autonavi.png "/></div> ',
+    );
+    info.push('<div style="padding:0px 0px 0px 4px;"><b>高德软件</b>');
+    info.push('电话 : 010-84107000   邮编 : 100102');
+    info.push('地址 :北京市朝阳区望京阜荣街10号首开广场4层</div></div>');
+    const infoWindow = new AMap.InfoWindow({
+        content: info.join('<br/>'), //使用默认信息窗体框样式，显示信息内容
+        offset: new AMap.Pixel(-2, -12),
+    });
+    infoWindow.open(MapRef.value, data);
+};
+
+const mapSearch = () => {};
+const mapSearchChange = () => {
+    console.log('searchChange !!!');
+};
+</script>
+
+<style lang="less" scoped>
+.region-map {
+    height: 100%;
+    width: 100%;
+    padding: 4px;
+}
+.tree-card {
+    min-width: 0;
+    word-wrap: break-word;
+    background-color: #fff;
+    background-clip: border-box;
+    border-radius: 0.25rem;
+    width: 260px;
+    height: 700px;
+    border-width: 0;
+    opacity: 0.85;
+    border-radius: 0.4rem;
+    box-shadow: 0 2px 6px 0 rgba(114, 124, 245, 0.5);
+    position: absolute;
+    top: 86px;
+    left: 245px;
+}
+
+.search-card {
+    display: flex;
+    flex-direction: row;
+    min-width: 0;
+    word-wrap: break-word;
+    background-color: #fff;
+    background-clip: border-box;
+    border-radius: 0.25rem;
+    width: 360px;
+    height: 48px;
+    border-width: 0;
+    opacity: 0.85;
+    border-radius: 0.4rem;
+    box-shadow: 0 2px 6px 0 rgba(114, 124, 245, 0.5);
+    position: absolute;
+    top: 100px;
+    right: 650px;
+}
+.vehicleType-card {
+    min-width: 0;
+    word-wrap: break-word;
+    background-color: #657aaf;
+    background-clip: border-box;
+    border-radius: 0.25rem;
+    width: 420px;
+    height: 40px;
+    border-width: 0;
+    opacity: 0.92;
+    border-radius: 0.4rem;
+    box-shadow: 0 2px 6px 0 rgba(234, 235, 243, 0.5);
+    position: absolute;
+    top: 104px;
+    right: 200px;
+}
+
+.radio-group {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    height: 100%;
+}
+.type-radio {
+    color: #fff;
+    margin-left: 30px;
+    line-height: 40px;
+}
+:deep(.radio-all) {
+    .ant-radio-inner {
+        // background-color: #1e90ff; /* 选中时的圆圈颜色 */
+        border-color: #1e90ff;
+    }
+    .ant-radio-inner::hover {
+        background-color: #1e90ff; /* 选中时的圆圈颜色 */
+        border-color: #1e90ff;
+    }
+    .ant-radio-inner::after {
+        background-color: #1e90ff; /* 改变选中标记的颜色 */
+    }
+}
+:deep(.radio-Alarm) {
+    .ant-radio-inner {
+        // background-color: #ffa502; /* 选中时的圆圈颜色 */
+        border-color: #ffa502;
+    }
+    .ant-radio-inner::hover {
+        background-color: #ffa502; /* 选中时的圆圈颜色 */
+        border-color: #ffa502;
+    }
+    .ant-radio-inner::after {
+        background-color: #ffa502; /* 改变选中标记的颜色 */
+    }
+}
+
+:deep(.radio-Fault) {
+    .ant-radio-inner {
+        // background-color: #ff4757; /* 选中时的圆圈颜色 */
+        border-color: #ff4757;
+    }
+    .ant-radio-inner::hover {
+        background-color: #ff4757; /* 选中时的圆圈颜色 */
+        border-color: #ff4757;
+    }
+    .ant-radio-inner::after {
+        background-color: #ff4757; /* 改变选中标记的颜色 */
+    }
+}
+</style>
---
+++ src/views/vehicle/LocationMonitoring/VehicleMap/mapTree/index.vue
@@ -0,0 +1,162 @@
+<template>
+    <div class="left-contain">
+        <j-input
+            placeholder="请输入"
+            v-model:value="searchValue"
+            @pressEnter="search"
+            @change="searchChange"
+        >
+            <template #suffix>
+                <AIcon type="SearchOutlined" @click="search" />
+            </template>
+        </j-input>
+        <div class="listBox">
+            <j-tree
+                :tree-data="listData"
+                v-if="listData.length"
+                :fieldNames="{ title: 'name', key: 'id', children: 'children' }"
+                blockNode
+                :selectedKeys="selectedKeys"
+                :showLine="{ showLeafIcon: false }"
+            >
+                <template #title="item">
+                    <div
+                        class="treeItem"
+                        @click="() => selectGroup(item.data.id)"
+                    >
+                        <template v-if="!item?.children">
+                            <div class="itemText">
+                                <Ellipsis style="width: calc(100%-10px)">{{
+                                    item.name
+                                }}</Ellipsis>
+                            </div>
+                        </template>
+                        <template v-else>
+                            <Ellipsis style="width: calc(100%-10px)">{{
+                                item.name
+                            }}</Ellipsis>
+                        </template>
+                    </div>
+                </template>
+            </j-tree>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { queryRoleGroup } from '@/api/system/role';
+const emit = defineEmits(['selectData']);
+const listData: any = ref([
+    {
+        name: '全部',
+        id: 'map_total',
+        children: [],
+    },
+]);
+
+const treeData = [
+    {
+        name: 'A分组',
+        id: 'type_a',
+        children: [
+            { name: 'Aa企业(异常:20/100)', id: 'type_aa' },
+            { name: 'Ab企业(异常:20/100)', id: 'type_ab' },
+        ],
+    },
+    {
+        name: 'B分组',
+        id: 'type_b',
+        children: [{ name: 'Ba企业(异常:20/100)', id: 'type_ba' }],
+    },
+    {
+        name: 'C分组',
+        id: 'type_c',
+        children: [{ name: 'Ca企业(异常:20/100)', id: 'type_ca' }],
+    },
+    {
+        name: 'D分组',
+        id: 'type_d',
+        children: [
+            { name: 'Da企业(异常:20/100)', id: 'type_da' },
+            { name: 'Db企业(异常:20/100)', id: 'type_db' },
+        ],
+    },
+];
+
+const selectedKeys = ref<string[]>(['map_total']);
+const searchValue = ref();
+const queryGroup = async (select?: Boolean, searchName?: string) => {
+    const params = searchName
+        ? {
+              sorts: [{ name: 'createTime', order: 'desc' }],
+              terms: [
+                  {
+                      terms: [
+                          {
+                              value: '%' + searchName + '%',
+                              termType: 'like',
+                              column: 'name',
+                          },
+                      ],
+                  },
+              ],
+          }
+        : { sorts: [{ name: 'createTime', order: 'desc' }] };
+    const req: any = await queryRoleGroup(params);
+    if (req.status === 200) {
+        listData.value = [
+            {
+                name: '全部',
+                id: 'map_total',
+                children: [],
+            },
+            ...treeData,
+        ];
+        // if (req.result[req.result.length - 1].id === 'default_group') {
+        //     req.result.unshift(req.result[req.result.length - 1]);
+        //     req.result.pop();
+        // }
+        // if(req.result.length && select){
+        //     selectGroup(req.result[0].id)
+        // }
+    }
+};
+
+const search = () => {
+    queryGroup(true, searchValue.value);
+};
+const searchChange = () => {
+    if (searchValue.value === '') {
+        queryGroup();
+    }
+};
+const selectGroup = (id: string) => {
+    selectedKeys.value = [id];
+    id === 'map_total'
+        ? emit('selectData', '')
+        : emit('selectData', selectedKeys.value);
+};
+
+onMounted(() => {
+    queryGroup(true);
+});
+</script>
+<style lang="less" scoped>
+.controls {
+    margin: 10px 0;
+}
+
+.treeItem {
+    display: flex;
+    justify-content: space-between;
+
+    .itemText {
+        line-height: 32px;
+        max-width: 60%;
+    }
+}
+
+.listBox {
+    margin: 10px 0;
+}
+</style>
