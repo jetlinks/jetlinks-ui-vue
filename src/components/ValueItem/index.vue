050253410d371efa6e334176a4beb722629a4a28
leiqiaochu <leiqiaochu@aossci.com>
2024-07-18 17:42:00 +0800

    fix: bug#28118【Dueros】动作映射-指令类型为调用功能，参数列表值输入框与类型不一致

--- src/components/ValueItem/index.vue
+++ src/components/ValueItem/index.vue
@@ -28,7 +28,7 @@
             allowClear
             showTime
             valueFormat="YYYY-MM-DD HH:mm:ss"
-            style="width: 100%;z-index: 800"
+            style="width: 100%;z-index: 1071"
             v-bind="extra"
             :getPopupContainer="getPopupContainer"
             @change='dateChange'
