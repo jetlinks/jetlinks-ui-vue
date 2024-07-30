36cc1da5e4578ad9ac9b58793c65acca6c2f2a6c
leiqiaochu <leiqiaochu@aossci.com>
2024-07-30 14:41:48 +0800

    fix: bug#27692 【采集器】采集器的点位数据数组类型时，写入数据不能进行写入数组数据

--- src/views/DataCollect/Collector/Point/components/WritePoint/index.vue
+++ src/views/DataCollect/Collector/Point/components/WritePoint/index.vue
@@ -38,57 +38,75 @@
                         required: true,
                         message: `请输入${data.name}`,
                     },
+                    {
+                        validator: validatorOpc_UA,
+                        trigger: 'change',
+                    },
                 ]"
                 v-else-if="data.provider === 'OPC_UA'"
             >
-                <j-input-number
-                    v-if="
-                        [
-                            'double',
-                            'float',
-                            'llong',
-                            'long',
-                            'integer',
-                            'short',
-                        ].includes(valueType)
-                    "
-                    style="width: 100%"
-                    placeholder="请输入"
-                    v-model:value="formData.value"
-                />
-                <j-select
-                    v-else-if="['boolean'].includes(valueType)"
-                    style="width: 100%"
-                    v-model:value="formData.value"
-                    :options="[
-                        {
-                            label: '是',
-                            value: true,
-                        },
-                        {
-                            label: '否',
-                            value: false,
-                        },
-                    ]"
-                    placeholder="请选择"
-                    allowClear
-                    show-search
-                    :filter-option="filterOption"
-                />
-                <j-date-picker
-                    v-else-if="['datetime'].includes(valueType)"
-                    style="width: 100%"
-                    format="YYYY-MM-DD HH:mm:ss"
-                    show-time
-                    placeholder="请选择"
-                    @change="onChange"
-                />
+                <template v-for="(i, index) in formData.value">
+                    <div style="margin-top: 10px">
+                        <j-input-number
+                            v-if="
+                                [
+                                    'double',
+                                    'float',
+                                    'llong',
+                                    'long',
+                                    'integer',
+                                    'short',
+                                ].includes(valueType)
+                            "
+                            style="width: 100%"
+                            placeholder="请输入"
+                            v-model:value="formData.value[index]"
+                        />
+                        <j-select
+                            v-else-if="['boolean'].includes(valueType)"
+                            style="width: 100%"
+                            v-model:value="formData.value[index]"
+                            :options="[
+                                {
+                                    label: '是',
+                                    value: true,
+                                },
+                                {
+                                    label: '否',
+                                    value: false,
+                                },
+                            ]"
+                            placeholder="请选择"
+                            allowClear
+                            show-search
+                            :filter-option="filterOption"
+                        />
+                        <j-date-picker
+                            v-else-if="['datetime'].includes(valueType)"
+                            style="width: 100%"
+                            format="YYYY-MM-DD HH:mm:ss"
+                            show-time
+                            placeholder="请选择"
+                            @change="(value: Dayjs, dateString: string)=>onChange(value,dateString,index)"
+                        />
 
-                <j-input
-                    v-else
-                    placeholder="请输入"
-                    v-model:value="formData.value"
-                />
+                        <j-input
+                            v-else
+                            placeholder="请输入"
+                            v-model:value="formData.value[index]"
+                        />
+                    </div>
+                </template>
+                <div style="margin-top: 10px">
+                    <a-button @click="formData.value.push('')">+</a-button>
+                    <a-button
+                        v-if="formData.value.length !== 1"
+                        @click="
+                            formData.value.splice(formData.value.length - 1, 1)
+                        "
+                        >-</a-button
+                    >
+                </div>
             </j-form-item>
             <j-form-item
                 :label="data.name"
@@ -292,34 +310,51 @@ const s7Type = [
 const emit = defineEmits(['change']);
 const loading = ref(false);
 const formRef = ref<FormInstance>();
-
-const validateHex = async (rule:any, value:any) => {
-    return /^0[xX][0-9A-Fa-f]+$|^[0-9A-Fa-f]+$/.test(value)
-        ? Promise.resolve()
-        : Promise.reject('请输入16进制');
-};
-const collectorId = props.data.collectorId;
-const pointId: string = props.data.id;
-
 const priority = new Array(16);
 const formData = ref({
-    value: '',
+    value: props.data.provider === 'OPC_UA' ? [''] : '',
     others: {
         priority: 1,
     },
 });
 
-const onChange = (value: Dayjs, dateString: string) => {
-    formData.value.value = dateString;
+const validateHex = async (rule: any, value: any) => {
+    return /^0[xX][0-9A-Fa-f]+$|^[0-9A-Fa-f]+$/.test(value)
+        ? Promise.resolve()
+        : Promise.reject('请输入16进制');
+};
+
+const validatorOpc_UA = async (rule: any, value: any) => {
+    console.log(value);
+    return value.length &&
+        value.every((i) => {
+            return i;
+        })
+        ? Promise.resolve()
+        : Promise.reject(`请输入${props.data.name}`);
+};
+
+const collectorId = props.data.collectorId;
+const pointId: string = props.data.id;
+
+const onChange = (value: Dayjs, dateString: string, index?: number) => {
+    if (props.data.provider === 'OPC_UA') {
+        formData.value.value[index] = dateString;
+    } else {
+        formData.value.value = dateString;
+    }
 };
 
 const handleOk = async () => {
-    const data = await formRef.value?.validate();
+    const data:any = await formRef.value?.validate();
     // if (['hex'].includes(valueType)) {
     //     if (data?.value) {
     //         data.value = convertHexToDecimal(data?.value);
     //     }
     // }
+    if (props.data.provider === 'OPC_UA') {
+        data.value = data.value.length === 1 ?  data.value[0] : data.value
+    }
     const params: any = {
         ...data,
         pointId,
