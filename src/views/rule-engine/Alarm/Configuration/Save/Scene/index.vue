51a0e04583492353693fcc103662b8e99e81ea5c
XieYongHong <18010623010@163.com>
2024-08-13 11:56:22 +0800

    fix: 【告警配置】兼容2.1绑定与取消绑定场景联动

--- src/views/rule-engine/Alarm/Configuration/Save/Scene/index.vue
+++ src/views/rule-engine/Alarm/Configuration/Save/Scene/index.vue
@@ -46,6 +46,7 @@
           :showBranches="false"
           :showBindTags="true"
           :showRule="false"
+          :showHistory="false"
           @click="handleView(slotProps)"
         >
           <div class="scene-view">
@@ -116,32 +117,32 @@ const {data: activeKeys, reload} = useRequest(queryBindScene, {
   defaultValue: {}
 })
 
-const getActions = (
-  data: Partial<Record<string, any>>,
-  type: 'card' | 'table',
-): ActionsType[] => {
-  if (!data) return [];
-  const actions: ActionsType[] = [
-    {
-      key: 'action',
-      text: '解绑',
-      icon: 'DisconnectOutlined',
-      popConfirm: {
-        title: '确认解绑？',
-        onConfirm: async () => {
-          // const res = await unbindScene(id, [data.id], data.branchIndex);
-          const res = await unbindScene(id, [data.id]);
-          if (res.status === 200) {
-            onlyMessage('操作成功');
-            actionRef.value.reload();
-          }
-          return
-        },
-      },
-    },
-  ];
-  return actions;
-};
+// const getActions = (
+//   data: Partial<Record<string, any>>,
+//   type: 'card' | 'table',
+// ): ActionsType[] => {
+//   if (!data) return [];
+//   const actions: ActionsType[] = [
+//     {
+//       key: 'action',
+//       text: '解绑',
+//       icon: 'DisconnectOutlined',
+//       popConfirm: {
+//         title: '确认解绑？',
+//         onConfirm: async () => {
+//           // const res = await unbindScene(id, [data.id], data.branchIndex);
+//           const res = await unbindScene(id, [data.id]);
+//           if (res.status === 200) {
+//             onlyMessage('操作成功');
+//             actionRef.value.reload();
+//           }
+//           return
+//         },
+//       },
+//     },
+//   ];
+//   return actions;
+// };
 
 const queryTable = (_terms: any) => {
   return query(_terms, id)
