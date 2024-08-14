ad7a74f4b42f7dbea3c758c828509dc8c2fba1dd
zq1571865676 <1571865676@qq.com>
2024-08-14 18:12:33 +0800

    fix: 优化清洗记录详情数据

--- src/views/data-report/CleaningRecord/Modal/index.vue
+++ src/views/data-report/CleaningRecord/Modal/index.vue
@@ -13,32 +13,66 @@
                     ><j-input-number
                         v-model:value="form.maxValue"
                         :disabled="isDisabled"
-                        style="width: 90%"
+                        style="width: 95%"
                 /></j-form-item>
-                <j-form-item label="最小值" v-if="form.mimValue ? true : false"
+                <j-form-item label="最小值" v-if="form.minValue ? true : false"
                     ><j-input-number
-                        v-model:value="form.mimValue"
+                        v-model:value="form.minValue"
                         :disabled="isDisabled"
-                        style="width: 90%"
+                        style="width: 95%"
                 /></j-form-item>
-                <j-form-item
-                    label="异常值"
-                    v-if="form.exceptionValue ? true : false"
-                    ><j-input-number
-                        v-model:value="form.exceptionValue"
-                        :disabled="isDisabled"
-                        style="width: 90%"
-                /></j-form-item>
-                <j-form-item
-                    label="无效值"
-                    v-if="form.invalidValue ? true : false"
-                    ><j-input-number
-                        v-model:value="form.invalidValue"
-                        :disabled="isDisabled"
-                        style="width: 90%"
-                /></j-form-item>
+                <div
+                    class="input-group"
+                    style="width: 95%"
+                    v-if="
+                        form.exceptionKey | form.exceptionValue ? true : false
+                    "
+                >
+                    <p class="label">异常值</p>
+                    <div class="content">
+                        <j-form-item name="exceptionKey">
+                            <j-input
+                                v-model:value="form.exceptionKey"
+                                :disabled="isDisabled"
+                                placeholder="异常值"
+                            />
+                        </j-form-item>
+                        <span>-</span>
+                        <j-form-item name="exceptionValue">
+                            <j-input
+                                v-model:value="form.exceptionValue"
+                                :disabled="isDisabled"
+                                placeholder="转换后的异常值"
+                            />
+                        </j-form-item>
+                    </div>
+                </div>
+                <div
+                    class="input-group"
+                    style="width: 95%"
+                    v-if="form.invalidKey | form.invalidValue ? true : false"
+                >
+                    <p class="label">无效值</p>
+                    <div class="content">
+                        <j-form-item name="invalidKey">
+                            <j-input
+                                v-model:value="form.invalidKey"
+                                :disabled="isDisabled"
+                                placeholder="请输入无效值"
+                            />
+                        </j-form-item>
+                        <span>-</span>
+                        <j-form-item name="invalidValue">
+                            <j-input
+                                v-model:value="form.invalidValue"
+                                :disabled="isDisabled"
+                                placeholder="请输入转换后的无效值"
+                            />
+                        </j-form-item>
+                    </div>
+                </div>
             </j-form-item>
-            <j-form-item label="上报数据" class="item_title">
+            <j-form-item label="上报数据" class="item_title" style="width: 95%">
                 <j-textarea
                     v-model:value="form.originJson"
                     placeholder="这里回显设备上报数据json"
@@ -66,13 +100,27 @@ const form = ref<any>({});
  */
 const show = (data: any) => {
     visible.value = true;
-    form.value = {
-        maxValue: data?.cleanRule?.maxValue,
-        mimValue: data?.cleanRule?.mimValue,
-        exceptionValue: data?.cleanRule?.exceptionValue,
-        invalidValue: data?.cleanRule?.invalidValue,
-        originJson: data?.originJson,
-    };
+    if (data.cleanRule) {
+        // console.log('data.cleanRule', typeof data.cleanRule);
+        const cleanRuleData = JSON.parse(data.cleanRule);
+        form.value = {
+            maxValue: cleanRuleData?.maxValue,
+            minValue: cleanRuleData?.minValue,
+            exceptionKey: cleanRuleData?.exceptionKey,
+            exceptionValue: cleanRuleData?.exceptionValue,
+            invalidKey: cleanRuleData?.invalidKey,
+            invalidValue: cleanRuleData?.invalidValue,
+            originJson: data?.originJson,
+        };
+    } else {
+        form.value = {
+            maxValue: undefined,
+            minValue: undefined,
+            exceptionValue: undefined,
+            invalidValue: undefined,
+            originJson: data?.originJson,
+        };
+    }
 };
 
 defineExpose({
@@ -87,6 +135,24 @@ const close = () => {
 <style lang="less" scoped>
 .ant-form-item {
     margin-bottom: 12px;
+}
+
+.input-group {
+    .content {
+        display: flex;
+        gap: 16px;
+        & > span {
+            line-height: 32px;
+        }
+
+        :deep(.ant-form-item) {
+            flex-grow: 1;
+
+            .ant-form-item-control {
+                width: 100% !important;
+            }
+        }
+    }
 }
 :deep(.item_title > .ant-form-item-label > label) {
     font-size: 16px;
