486c08da57e45287844ab458ec69e60760a0f3a1
hear. <1175160654@qq.com>
2024-08-23 17:52:33 +0800

    feat: 抓拍计划列表接口联调

---
+++ src/views/media/TimingCapture/Plan/Add/index.vue
@@ -0,0 +1,54 @@
+<template>
+    <a-modal
+        visible
+        title="新增计划"
+        @ok="submitData"
+        @cancel="emit('closeModal')"
+    >
+        <a-form ref="formRef" layout="vertical" :model="formData">
+            <a-form-item
+                label="计划名称"
+                name="name"
+                :rules="[
+                    {
+                        required: true,
+                        message: '请输入计划名称',
+                    },
+                    {
+                        max: 64,
+                        message: '最多可输入64个字符',
+                    },
+                ]"
+            >
+                <a-input
+                    v-model:value="formData.name"
+                    placeholder="请输入计划名称"
+                ></a-input>
+            </a-form-item>
+        </a-form>
+    </a-modal>
+</template>
+
+<script setup>
+import { savePlan } from '@/api/media/auto';
+const emit = defineEmits(['closeModal']);
+const formRef = ref();
+const formData = reactive({
+    name: '',
+});
+const submitData =async () => {
+    const res = await formRef.value?.validate();
+    if(res){
+        const resp = await savePlan({
+            ...res,
+            type:'photo'
+        });
+        if (resp.status === 200) {
+            onlyMessage('操作成功!');
+            emit('closeModal');
+        }
+    }
+}
+
+</script>
+<style lang="less" scoped></style>
--- src/views/media/TimingCapture/Plan/index.vue
+++ src/views/media/TimingCapture/Plan/index.vue
@@ -1,119 +1,128 @@
 <template>
-  <page-container>
-      <pro-search
-          :columns="columns"
-          target="auto-video-plan"
-          @search="handleSearch"
-      />
-
-      <FullPage>
-          <JProTable
-              ref="tableRef"
-              :columns="columns"
-              model="CARD"
-              :defaultParams="{
-                  sorts: [{ name: 'createTime', order: 'desc' }],
-              }"
-              :params="params"
-          >
-              <template #headerTitle>
-                  <PermissionButton
-                      type="primary"
-                      @click="handleAdd"
-                      hasPermission="media/Device:add"
-                  >
-                      <template #icon><AIcon type="PlusOutlined" /></template>
-                      新增计划
-                  </PermissionButton>
-              </template>
-              <template #card="slotProps">
-                  <CardBox
-                      @click="() => handleClick(slotProps)"
-                      :value="slotProps"
-                      :actions="getActions(slotProps, 'card')"
-                      v-bind="slotProps"
-                      :showStatus="true"
-                      :status="slotProps.state.value"
-                      :statusText="slotProps.state.text"
-                      :statusNames="{
-                          online: 'processing',
-                          offline: 'error',
-                      }"
-                  >
-                      <template #img>
-                          <slot name="img">
-                              <img :src="getImage('/device-media.png')" />
-                          </slot>
-                      </template>
-                      <template #content>
-                          <Ellipsis style="width: calc(100% - 100px)">
-                              <span style="font-size: 16px; font-weight: 700">
-                                  {{ slotProps.name }}
-                              </span>
-                          </Ellipsis>
-                          <j-row>
-                              <j-col :span="8">
-                                  <div class="card-item-content-text">
-                                      计划ID
-                                  </div>
-                                  <Ellipsis style="width: calc(100% - 20px)">
-                                      <div>{{ slotProps.manufacturer }}</div>
-                                  </Ellipsis>
-                              </j-col>
-                              <j-col :span="8">
-                                  <div class="card-item-content-text">
-                                      创建时间
-                                  </div>
-                                  <div>{{ slotProps.channelNumber }}</div>
-                              </j-col>
-                              <j-col :span="8">
-                                  <div class="card-item-content-text">
-                                      录像保存周期（天）
-                                  </div>
-                                  <Ellipsis
-                                      style="width: calc(100% - 20px)"
-                                      >{{ slotProps.model }}</Ellipsis
-                                  >
-                              </j-col>
-                          </j-row>
-                      </template>
-                      <template #actions="item">
-                          <PermissionButton
-                              :disabled="item.disabled"
-                              :popConfirm="item.popConfirm"
-                              :tooltip="{
-                                  ...item.tooltip,
-                              }"
-                              @click="item.onClick"
-                              :hasPermission="
-                                  'media/Device:' +
-                                  (item.key !== 'updateChannel'
-                                      ? item.key
-                                      : 'update')
-                              "
-                          >
-                              <AIcon
-                                  type="DeleteOutlined"
-                                  v-if="item.key === 'delete'"
-                              />
-                              <template v-else>
-                                  <AIcon :type="item.icon" />
-                                  <span>{{ item?.text }}</span>
-                              </template>
-                          </PermissionButton>
-                      </template>
-                  </CardBox>
-              </template>
-          </JProTable>
-      </FullPage>
-  </page-container>
-  <AddPlan v-if="addVisible" @closeModal="addVisible = false"></AddPlan>
+    <page-container>
+        <pro-search
+            :columns="columns"
+            target="auto-video-plan"
+            @search="handleSearch"
+        />
+        <FullPage>
+            <JProTable
+                ref="tableRef"
+                :columns="columns"
+                :request="queryList"
+                model="CARD"
+                :gridColumn="2"
+                :defaultParams="{
+                    sorts: [{ name: 'createTime', order: 'desc' }],
+                    terms: [{ column: 'type', value: 'photo',termType:'eq' }],
+                }"
+                :params="params"
+            >
+                <template #headerTitle>
+                    <PermissionButton
+                        type="primary"
+                        @click="handleAdd"
+                        hasPermission="media/Device:add"
+                    >
+                        <template #icon><AIcon type="PlusOutlined" /></template>
+                        新增计划
+                    </PermissionButton>
+                </template>
+                <template #card="slotProps">
+                    <CardBox
+                        @click="() => handleClick(slotProps)"
+                        :value="slotProps"
+                        :actions="getActions(slotProps, 'card')"
+                        v-bind="slotProps"
+                        :showStatus="true"
+                        :status="slotProps.state.value"
+                        :statusText="
+                            slotProps.state.value === 'enabled'
+                                ? '正常'
+                                : '禁用'
+                        "
+                        :statusNames="{
+                            enabled: 'processing',
+                            disabled: 'error',
+                        }"
+                    >
+                        <template #img>
+                            <slot name="img">
+                                <img :src="getImage('/device-media.png')" />
+                            </slot>
+                        </template>
+                        <template #content>
+                            <Ellipsis style="width: calc(100% - 100px)">
+                                <span style="font-size: 16px; font-weight: 700">
+                                    {{ slotProps.name }}
+                                </span>
+                            </Ellipsis>
+                            <a-row :gutter="16" style="margin-top: 10px;">
+                                <a-col :span="8">
+                                    <div class="card-item-content-text">
+                                        计划ID
+                                    </div>
+                                    <Ellipsis style="width: calc(100% - 20px)">
+                                        <div>{{ slotProps.id }}</div>
+                                    </Ellipsis>
+                                </a-col>
+                                <a-col :span="8">
+                                    <div class="card-item-content-text">
+                                        创建时间
+                                    </div>
+                                    <span>{{
+                                        dayjs(slotProps?.createTime).format(
+                                            'YYYY-MM-DD HH:mm:ss',
+                                        )
+                                    }}</span>
+                                </a-col>
+                                <a-col :span="8">
+                                    <div class="card-item-content-text">
+                                        录像保存周期（天）
+                                    </div>
+                                    <Ellipsis
+                                        style="width: calc(100% - 20px)"
+                                        >{{
+                                            slotProps?.others?.retention
+                                        }}</Ellipsis
+                                    >
+                                </a-col>
+                            </a-row>
+                        </template>
+                        <template #actions="item">
+                            <PermissionButton
+                                :disabled="item.disabled"
+                                :popConfirm="item.popConfirm"
+                                :tooltip="{
+                                    ...item.tooltip,
+                                }"
+                                @click="item.onClick"
+                                :hasPermission="true"
+                            >
+                                <AIcon
+                                    type="DeleteOutlined"
+                                    v-if="item.key === 'delete'"
+                                />
+                                <template v-else>
+                                    <AIcon :type="item.icon" />
+                                    <span>{{ item?.text }}</span>
+                                </template>
+                            </PermissionButton>
+                        </template>
+                    </CardBox>
+                </template>
+            </JProTable>
+        </FullPage>
+    </page-container>
+    <AddPlan v-if="addVisible" @closeModal="addVisible = false"></AddPlan>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { getImage, onlyMessage } from '@/utils/comm';
 import { useMenuStore } from 'store/menu';
-import AddPlan from '@/views/media/AutoVideo/Plan/Add/index.vue';
+import AddPlan from '@/views/media/TimingCapture/Plan/Add/index.vue';
+import { queryList ,controlPlan,deletePlan} from '@/api/media/auto';
+import dayjs from 'dayjs';
 
 const addVisible = ref(false);
 const params = ref();
@@ -121,110 +130,131 @@ const menuStory = useMenuStore();
 const tableRef = ref();
 
 const columns = [
-  {
-      title: 'ID',
-      dataIndex: 'id',
-      key: 'id',
-      width: 200,
-      fixed: 'left',
-      search: {
-          type: 'string',
-      },
-      ellipsis: true,
-  },
-  {
-      title: '名称',
-      dataIndex: 'name',
-      key: 'name',
-      search: {
-          type: 'string',
-          first: true,
-      },
-      ellipsis: true,
-  },
-  {
-      title: '状态',
-      dataIndex: 'state',
-      key: 'state',
-      width: 100,
-      search: {
-          type: 'select',
-          options: [
-              { label: '禁用', value: 'notActive' },
-              { label: '离线', value: 'offline' },
-              { label: '在线', value: 'online' },
-          ],
-      },
-  },
+    {
+        title: 'ID',
+        dataIndex: 'id',
+        key: 'id',
+        width: 200,
+        fixed: 'left',
+        search: {
+            type: 'string',
+        },
+        ellipsis: true,
+    },
+    {
+        title: '名称',
+        dataIndex: 'name',
+        key: 'name',
+        search: {
+            type: 'string',
+            first: true,
+        },
+        ellipsis: true,
+    },
+    {
+        title: '状态',
+        dataIndex: 'state',
+        key: 'state',
+        width: 100,
+        search: {
+            type: 'select',
+            options: [
+                { label: '启用', value: 'enabled' },
+                { label: '禁用', value: 'disabled' },
+            ],
+        },
+    },
 ];
 
 const getActions = (data, type) => {
-  if (!data) return [];
-  const actions = [
-      {
-          key: 'update',
-          text: '编辑',
-          tooltip: {
-              title: '编辑',
-          },
-          icon: 'EditOutlined',
-          onClick: () => {
-              menuStory.jumpPage('media/AutoVideo/Plan/Detail', {
-                  id: data.id,
-              });
-          },
-      },
-      {
-          key: 'view',
-          text: '查看',
-          tooltip: {
-              title: '查看',
-          },
-          icon: 'EyeOutlined',
-          onClick: () => {
-              menuStory.jumpPage('device/Instance/Detail', { id: data.id });
-          },
-      },
-      {
-          key: 'delete',
-          text: '删除',
-          tooltip: {
-              title:
-                  data.state.value === 'online' ? '在线设备无法删除' : '删除',
-          },
-          disabled: data.state.value === 'online',
-          popConfirm: {
-              title: '确认删除?',
-              onConfirm: () => {
-                  const response = DeviceApi.del(data.id);
-                  response.then((resp) => {
-                      if (resp.status === 200) {
-                          onlyMessage('操作成功！');
-                          tableRef.value?.reload();
-                      } else {
-                          onlyMessage('操作失败！', 'error');
-                      }
-                  });
-                  return response;
-              },
-          },
-          icon: 'DeleteOutlined',
-      },
-  ];
-  return actions;
+    if (!data) return [];
+    const actions = [
+        {
+            key: 'update',
+            text: '编辑',
+            tooltip: {
+                title: '编辑',
+            },
+            icon: 'EditOutlined',
+            onClick: () => {
+                menuStory.jumpPage('media/AutoVideo/Plan/Detail', {
+                    id: data.id,
+                });
+            },
+        },
+        {
+            key: 'action',
+            text: data.state.value === 'enabled' ? '禁用' : '启用',
+            tooltip: {
+                title: data.state.value === 'enabled' ? '禁用' : '启用',
+            },
+            icon: data.state.value === 'enabled' ? 'StopOutlined' : 'CheckCircleOutlined',
+            popConfirm: {
+                title: `确认${data.state.value === 0 ? '禁用' : '启用'}?`,
+                onConfirm: () => {
+                    let response = undefined;
+                    if (data.state.value === 'enabled') {
+                        response = controlPlan(data.id,{
+                            state:'disabled'
+                        });
+                    } else {
+                        response = controlPlan(data.id,{
+                            state:'enabled'
+                        });
+                    }
+                    response.then((res) => {
+                        if (res && res.status === 200) {
+                            onlyMessage('操作成功！');
+                            tableRef.value?.reload();
+                        } else {
+                            onlyMessage('操作失败！', 'error');
+                        }
+                    });
+                    return response;
+                },
+            },
+        },
+        {
+            key: 'delete',
+            text: '删除',
+            tooltip: {
+                title:
+                    data.state.value === 'enabled' ? '请先禁用计划' : '删除',
+            },
+            disabled: data.state.value === 'enabled',
+            popConfirm: {
+                title: '确认删除?',
+                onConfirm: () => {
+                    const response = deletePlan(data.id);
+                    response.then((resp) => {
+                        if (resp.status === 200) {
+                            onlyMessage('操作成功！');
+                            tableRef.value?.reload();
+                        } else {
+                            onlyMessage('操作失败！', 'error');
+                        }
+                    });
+                    return response;
+                },
+            },
+            icon: 'DeleteOutlined',
+        },
+    ];
+    return actions;
 };
 
 const handleAdd = () => {
-  menuStory.jumpPage(
-      'media/TimingCapture/Plan/Detail',
-      {
-          id: ':id',
-      },
-      { type: 'edit' },
-  );
+    addVisible.value = true;
+    // menuStory.jumpPage(
+    //     'media/TimingCapture/Plan/Detail',
+    //     {
+    //         id: ':id',
+    //     },
+    //     { type: 'edit' },
+    // );
 };
 const handleSearch = (e) => {
-  params.value = e;
+    params.value = e;
 };
 
 const handleClick = (data) => {};
