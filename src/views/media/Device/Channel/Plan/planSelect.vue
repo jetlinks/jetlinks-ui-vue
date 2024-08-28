ee712f6e9f9b80eed132364ebc8167f56956c927
hear. <1175160654@qq.com>
2024-08-28 14:53:13 +0800

    fix: 视频设备计划弹框

---
+++ src/views/media/Device/Channel/Plan/planSelect.vue
@@ -0,0 +1,57 @@
+<template>
+    <a-select
+        v-model:value="_value"
+        style="width: 200px"
+        :options="_options"
+        :field-names="{ label: 'name', value: 'id' }"
+        @change="(value, option) => onChange(value, option)"
+        placeholder="请选择计划"
+    />
+</template>
+
+<script setup lang="ts" name="planSelect">
+import { computed, ref, watch } from 'vue';
+
+const props = defineProps({
+    options: {
+        type: Array,
+        default: () => [],
+    },
+    selectKeys: {
+        type: Array,
+        default: () => [],
+    },
+    value: {
+        type: String,
+        default: undefined,
+    },
+});
+
+const _options = computed(() => {
+    const keys = props.selectKeys.map((i:any)=>{
+        if(i.id!==_value.value) return i.id
+    })
+    return props.options.filter((item:any)=>{
+        return !keys.includes(item.id)
+    });
+});
+
+const _value = ref('');
+
+const emits = defineEmits(['change', 'update:value']);
+
+const onChange = (value: string, option: any) => {
+    emits('change', option);
+    emits('update:value', value);
+};
+
+watch(
+    () => props.value,
+    () => {
+        _value.value = props.value;
+    },
+    { immediate: true },
+);
+</script>
+
+<style lang="less" scoped></style>
