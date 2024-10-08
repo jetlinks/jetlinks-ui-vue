2311979a4ccdab3cca00ca746117e3b830b73b5b
XieYongHong <18010623010@163.com>
2024-10-08 12:00:16 +0800

    update: 新增@vueuse/router插件
    * update: 新增@vueuse/router插件

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 docker build -t registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-vue:2.3.0-SNAPSHOT .
 docker push registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-vue:2.3.0-SNAPSHOT
-
