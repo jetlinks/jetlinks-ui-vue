3dd8363bad08f31978206b2abb16eb706eb6610b
XieYongHong <18010623010@163.com>
2024-10-14 19:21:59 +0800

    fix: 数采设备-数采映射设备禁用时传默认状态disabled

--- src/views/device/Instance/Detail/EdgeMap/PatchMapping.vue
+++ src/views/device/Instance/Detail/EdgeMap/PatchMapping.vue
@@ -220,6 +220,7 @@ const handleClick = async () => {
                 provider: treeList.value.find(
                     (it: any) => it.id === item.parentId,
                 )?.provider,
+                state: instanceStore.current.state.value === 'notActive' ? 'disabled' : null,
             }));
             params.push(...array);
         });
