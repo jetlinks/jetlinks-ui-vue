e6507e1706358d7e0fd6eb3214b630a5f10ed8d7
leiqiaochu <leiqiaochu@aossci.com>
2024-10-28 14:50:31 +0800

    feat: 视频设备新增agent类型

--- src/views/media/Device/const.ts
+++ src/views/media/Device/const.ts
@@ -2,7 +2,8 @@ export const PROVIDER_OPTIONS = [
     { label: 'GB/T28181', value: 'gb28181-2016' },
     { label: '固定地址', value: 'fixed-media' },
     { label: 'Onvif', value: 'onvif'},
-    { label: '插件', value: 'media-plugin'}
+    { label: '插件', value: 'media-plugin'},
+    { label: 'Agent接入', value: 'agent-device-gateway'}
 ]
 export const streamMode = [
     { label: 'UDP', value: 'UDP' },
@@ -13,5 +14,6 @@ export const providerType = {
     'gb28181-2016': 'GB/T28181',
     'fixed-media': '固定地址',
     'onvif': 'Onvif',
-    'media-plugin': '插件'
+    'media-plugin': '插件',
+    'agent-device-gateway':'Agent接入'
 };
