722205899378d184e954fdce1ba7fa0109931230
孙超 <54933585+sun-chaochao@users.noreply.github.com>
2024-11-06 17:13:57 +0800

    fix: 修改物联网卡的批量导出，同步状态功能
    * fix: 修改物联网卡
    
    * fix: 修改物联网卡的批量导出，同步状态功能

--- src/views/iot-card/CardManagement/Export.vue
+++ src/views/iot-card/CardManagement/Export.vue
@@ -1,65 +1,85 @@
 <template>
-    <!-- 导入 -->
-    <j-modal
-        :maskClosable="false"
-        :visible="true"
-        title="导出"
-        okText="确定"
-        cancelText="取消"
-        @ok="handleOk"
-        @cancel="handleCancel"
-    >
-        <div style="margin-top: 10px">
-            <j-space>
-                <span>文件格式：</span>
-                <j-radio-group
-                    v-model:value="type"
-                    placeholder="请选择文件格式"
-                    button-style="solid"
-                >
-                    <j-radio-button value="xlsx">xlsx</j-radio-button>
-                    <j-radio-button value="csv">csv</j-radio-button>
-                </j-radio-group>
-            </j-space>
-        </div>
-    </j-modal>
+  <!-- 导入 -->
+  <j-modal
+      :maskClosable="false"
+      :visible="true"
+      title="导出"
+      okText="确定"
+      cancelText="取消"
+      @ok="handleOk"
+      @cancel="handleCancel"
+      :confirmLoading="loading"
+  >
+    <div style="margin-top: 10px">
+      <j-space>
+        <span>文件格式：</span>
+        <j-radio-group
+            v-model:value="type"
+            placeholder="请选择文件格式"
+            button-style="solid"
+        >
+          <j-radio-button value="xlsx">xlsx</j-radio-button>
+          <j-radio-button value="csv">csv</j-radio-button>
+        </j-radio-group>
+      </j-space>
+    </div>
+  </j-modal>
 </template>
 
 <script setup lang="ts">
 import dayjs from 'dayjs';
-import { _export } from '@/api/iot-card/cardManagement';
-import { downloadFileByUrl } from '@/utils/utils';
+import {_export} from '@/api/iot-card/cardManagement';
+import {downloadFileByUrl} from '@/utils/utils';
+import {paramsEncodeQuery} from "@/utils/encodeQuery";
+import {LocalStore} from "@/utils/comm";
+import {TOKEN_KEY} from "@/utils/variable";
 
 const emit = defineEmits(['close']);
 
 const props = defineProps({
-    data: {
-        type: Object,
-        default: undefined,
-    },
+  data: {
+    type: Object,
+    default: undefined,
+  },
 });
 
 const type = ref<string>('xlsx');
+const loading = ref<boolean>(false);
 
 const handleOk = () => {
-    _export(type.value, props.data).then((res: any) => {
-        if (res) {
-            const blob = new Blob([res.data], { type: type.value });
-            const url = URL.createObjectURL(blob);
-            downloadFileByUrl(
-                url,
-                `物联卡管理-${dayjs(new Date()).format(
-                    'YYYY/MM/DD HH:mm:ss',
-                )}`,
-                type.value,
-            );
-            emit('close');
-        }
-    });
+  loading.value = true
+  const _params = paramsEncodeQuery(props.data);
+  const urlParams = new URLSearchParams()
+
+  Object.keys(_params).forEach(key => {
+    if (_params[key]) {
+      urlParams.append(key, _params[key])
+    }
+  })
+  const url = `${origin}/api/network/card/download.${type.value}/_query?:X_Access_Token=${LocalStore.get(TOKEN_KEY)}&${urlParams}`
+  window.open(url)
+  loading.value = false
+  emit('close');
+  // _export(type.value, props.data).then((res: any) => {
+  //   if (res) {
+  //     const blob = new Blob([res.data], {type: type.value});
+  //     const url = URL.createObjectURL(blob);
+  //     downloadFileByUrl(
+  //         url,
+  //         `物联卡管理-${dayjs(new Date()).format(
+  //             'YYYY/MM/DD HH:mm:ss',
+  //         )}`,
+  //         type.value,
+  //     );
+  //     emit('close');
+  //   }
+  // }).finally(() => {
+  //   loading.value = false
+  // })
 };
 
 const handleCancel = () => {
-    emit('close');
+  emit('close');
 };
 </script>
 
--- src/views/system/Calendar/Tags/index.vue
+++ src/views/system/Calendar/Tags/index.vue
@@ -1,84 +1,83 @@
 <template>
-    <div class="tagsContainer" ref="tags">
-        <PermissionButton
-            type="primary"
-            hasPermission="system/Calendar:add"
-            ghost
-            block
-            @click="addTag"
-            :disabled="rapidOn"
-            style="margin-bottom: 10px"
-        >
-            + 新增标签
-        </PermissionButton>
-        <div class="content">
-            <div v-for="i in tagsList" class="tag">
-                <div class="tagLeft">
-                    <div
-                        :style="{ background: i.color }"
-                        class="colorExtractor"
-                    ></div>
-                    <Ellipsis
-                        class="tagName"
-                        style="
-                            width: 120px;
-                            cursor: url('/images/calendar/hover.png'), pointer;
-                        "
-                        :id="i.id"
-                        >{{ i.name }}</Ellipsis
-                    >
-                </div>
-                <div class="controls">
-                    <PermissionButton
-                        type="text"
-                        hasPermission="system/Calendar:update"
-                        :disabled="i.disabled || rapidOn"
-                        :tooltip="{
+  <div class="tagsContainer" ref="tags">
+    <PermissionButton
+        type="primary"
+        hasPermission="system/Calendar:add"
+        ghost
+        block
+        @click="addTag"
+        :disabled="rapidOn"
+        style="margin-bottom: 10px"
+    >
+      + 新增标签
+    </PermissionButton>
+    <div class="content">
+      <div v-for="i in tagsList" class="tag">
+        <div class="tagLeft">
+          <div
+              :style="{ background: i.color }"
+              class="colorExtractor"
+          ></div>
+          <Ellipsis
+              class="tagName"
+              style="cursor: grabbing;"
+              :id="i.id"
+          >{{ i.name }}
+          </Ellipsis
+          >
+        </div>
+        <div class="controls">
+          <PermissionButton
+              type="text"
+              hasPermission="system/Calendar:update"
+              :disabled="i.disabled || rapidOn"
+              :tooltip="{
                             title: '编辑',
                         }"
-                        @click="() => editData(i)"
-                    >
-                        <template #icon>
-                            <AIcon type="EditOutlined" />
-                        </template>
-                    </PermissionButton>
-                    <PermissionButton
-                        type="text"
-                        hasPermission="system/Calendar:delete"
-                        :disabled="i.disabled || rapidOn"
-                        :tooltip="{
+              @click="() => editData(i)"
+          >
+            <template #icon>
+              <AIcon type="EditOutlined"/>
+            </template>
+          </PermissionButton>
+          <PermissionButton
+              type="text"
+              hasPermission="system/Calendar:delete"
+              :disabled="i.disabled || rapidOn"
+              :tooltip="{
                             title: '删除',
                         }"
-                        :popConfirm="{
+              :popConfirm="{
                             title: `确认删除？`,
                             onConfirm: () => deleteData(i.id),
                         }"
-                    >
-                        <template #icon>
-                            <AIcon type="DeleteOutlined" />
-                        </template>
-                    </PermissionButton>
-                </div>
-            </div>
+          >
+            <template #icon>
+              <AIcon type="DeleteOutlined"/>
+            </template>
+          </PermissionButton>
         </div>
+      </div>
     </div>
-    <EditTag
-        v-if="editVisible"
-        :edit-type="editType"
-        :editData="currentTag"
-        @close-edit-tag="editVisible = false"
-        @refresh="refreshTags"
-    />
+  </div>
+  <EditTag
+      v-if="editVisible"
+      :edit-type="editType"
+      :editData="currentTag"
+      @close-edit-tag="editVisible = false"
+      @refresh="refreshTags"
+  />
 </template>
 
 <script setup name="CalendarTags">
-import { queryTags, deleteTags, saveTagsColor } from '@/api/system/calendar';
-import { Draggable } from '@fullcalendar/interaction';
+import {queryTags, deleteTags, saveTagsColor} from '@/api/system/calendar';
+import {Draggable} from '@fullcalendar/interaction';
 import EditTag from './components/editTag.vue';
-import { onlyMessage } from '@/utils/comm';
-import { inject } from 'vue';
-import { omit } from 'lodash-es';
-import { useSystem } from '@/store/system';
+import {onlyMessage} from '@/utils/comm';
+import {inject} from 'vue';
+import {omit} from 'lodash-es';
+import {useSystem} from '@/store/system';
+
 const system = useSystem();
 const calendarTagColor = system.$state.calendarTagColor;
 const tagsMap = inject('tagsMap');
@@ -89,8 +88,8 @@ const tagsList = ref();
 const editType = ref();
 const currentTag = ref();
 const addTag = () => {
-    editVisible.value = true;
-    editType.value = 'add';
+  editVisible.value = true;
+  editType.value = 'add';
 };
 const buildInTag = ['weekend', 'holiday', 'workday'];
 const defaultColor = new Map();
@@ -99,121 +98,130 @@ defaultColor.set('holiday', 'rgb(161, 180, 204)');
 defaultColor.set('workday', 'rgba(105,177,255)');
 
 const createDrag = () => {
-    new Draggable(tags.value, {
-        itemSelector: '.tagName',
-        eventData: function (eventEl) {
-            return {
-                id: eventEl.id,
-                title: eventEl.innerText,
-                backgroundColor: calendarTagColor.get(eventEl.id) || '#000000',
-                color: '#000',
-                editable: false,
-            };
-        },
-    });
+  new Draggable(tags.value, {
+    itemSelector: '.tagName',
+    eventData: function (eventEl) {
+      return {
+        id: eventEl.id,
+        title: eventEl.innerText,
+        backgroundColor: calendarTagColor.get(eventEl.id) || '#000000',
+        color: '#000',
+        editable: false,
+      };
+    },
+  });
 };
 //获取标签列表
 const queryTagsData = async () => {
-    const res = await queryTags();
-    if (res.success) {
-        //获取用户添加的标签颜色及权限
-        await system.getTagsColor();
-        tagsList.value = res.result.map((i) => {
-            let color = '#000000';
-            let disabled = false;
-            if (calendarTagColor.has(i.id)) {
-                color = calendarTagColor.get(i.id);
-            }
-            if (buildInTag.includes(i.id)) {
-                disabled = true;
-                color = defaultColor.get(i.id);
-            }
-            return {
-                ...i,
-                color,
-                disabled,
-            };
-        });
-        tagsMap.value = tagsList.value.map((i) => {
-            return omit(i, ['disabled']);
-        });
-    }
+  const res = await queryTags();
+  if (res.success) {
+    //获取用户添加的标签颜色及权限
+    await system.getTagsColor();
+    tagsList.value = res.result.map((i) => {
+      let color = '#000000';
+      let disabled = false;
+      if (calendarTagColor.has(i.id)) {
+        color = calendarTagColor.get(i.id);
+      }
+      if (buildInTag.includes(i.id)) {
+        disabled = true;
+        color = defaultColor.get(i.id);
+      }
+      return {
+        ...i,
+        color,
+        disabled,
+      };
+    });
+    tagsMap.value = tagsList.value.map((i) => {
+      return omit(i, ['disabled']);
+    });
+  }
 };
 
 const refreshTags = () => {
-    editVisible.value = false;
-    queryTagsData();
+  editVisible.value = false;
+  queryTagsData();
 };
 
 const deleteData = async (id) => {
-    const res = await deleteTags([id]);
-    if (res.success) {
-        if (calendarTagColor.has(id)) {
-            calendarTagColor.delete(id);
-            let color = new Object();
-            calendarTagColor.forEach((item, key) => {
-                color[key] = item;
-            });
-            const deleteColor = await saveTagsColor(color);
-        }
-        onlyMessage('操作成功');
-        queryTagsData();
+  const res = await deleteTags([id]);
+  if (res.success) {
+    if (calendarTagColor.has(id)) {
+      calendarTagColor.delete(id);
+      let color = new Object();
+      calendarTagColor.forEach((item, key) => {
+        color[key] = item;
+      });
+      const deleteColor = await saveTagsColor(color);
     }
+    onlyMessage('操作成功');
+    queryTagsData();
+  }
 };
 
 const editData = (data) => {
-    editVisible.value = true;
-    editType.value = 'edit';
-    currentTag.value = data;
+  editVisible.value = true;
+  editType.value = 'edit';
+  currentTag.value = data;
 };
 onMounted(() => {
-    queryTagsData();
-    createDrag();
+  queryTagsData();
+  createDrag();
 });
 </script>
 <style lang="less" scoped>
 .tagsContainer {
-    width: 15%;
-    background-color: #fff;
-    padding: 10px;
-    border-right: 1px solid #cccccc;
-    .content {
-        overflow: auto;
-        height: 700px;
+  width: 300px;
+  background-color: #fff;
+  padding: 10px;
+  border-right: 1px solid #cccccc;
+
+  .content {
+    overflow-y: auto;
+    height: 700px;
+    width: 100%;
+  }
+
+  .tag {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 10px;
+    padding: 0 10px;
+    height: 32px;
+
+    .tagLeft {
+      display: flex;
+      padding-top: 6px;
+
+      .colorExtractor {
+        margin-top: 3px;
+        width: 16px;
+        height: 16px;
+        border-radius: 2px;
+        margin-right: 8px;
+      }
     }
-    .tag {
+
+    .controls {
+      display: none;
+      font-size: 14px;
+
+      :deep(.ant-btn-text) {
+        padding: 4px 8px;
+      }
+    }
+
+    &:hover {
+      .controls {
         display: flex;
-        justify-content: space-between;
-        margin-bottom: 10px;
-        padding: 0 10px;
-        height: 32px;
-        .tagLeft {
-            display: flex;
-            padding-top: 6px;
-            .colorExtractor {
-                margin-top: 3px;
-                width: 16px;
-                height: 16px;
-                border-radius: 2px;
-                margin-right: 8px;
-            }
-        }
-        .controls {
-            display: none;
-            font-size: 14px;
-            :deep(.ant-btn-text) {
-                padding: 4px 8px;
-            }
-        }
-        &:hover {
-            .controls {
-                display: flex;
-            }
-        }
+      }
     }
-    .tag:hover {
-        background-color: rgb(242, 242, 242);
-        border-radius: 4px;
-    }
+  }
+
+  .tag:hover {
+    background-color: rgb(242, 242, 242);
+    border-radius: 4px;
+  }
 }
 </style>
