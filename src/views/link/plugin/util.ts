b259b5f22803607bc3a70e9ee534f9a914c7d288
leiqiaochu <leiqiaochu@aossci.com>
2024-11-19 10:33:22 +0800

    fix: 批量管理 产品分类查询

--- src/views/link/plugin/util.ts
+++ src/views/link/plugin/util.ts
@@ -1,5 +1,8 @@
 export const TypeMap = {
   'deviceGateway': '设备接入网关',
-  'thingsManager': '物管理',
-  'media': '视频接入网关'
+  'collector': '数据采集',
+  'ruleEngine':'规则引擎',
+  'standalone':'独立运行',
+  'media':'视频接入网关',
+  'ai': 'AI插件'
 }
