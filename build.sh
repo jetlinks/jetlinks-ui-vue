693406f8f578abafb531d951f10ba8118f9c013f
XieYongHong <18010623010@163.com>
2024-11-22 11:38:24 +0800

    fix: 修改docker配置

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker build -t registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-vue:2.3.0-TEST .
-#docker push registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-vue:2.3.0-TEST
+docker push registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-vue:2.3.0-TEST
