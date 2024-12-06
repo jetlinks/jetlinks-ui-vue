9c4c44ba731c82aa66d5743b43de2e072d2bacd4
XieYongHong <18010623010@163.com>
2024-12-06 16:55:42 +0800

    fix: 仪表盘最新告警时间取值
    * fix: bug#31978【网络组件】网络组件编辑时，端口展示异常
    
    * fix: 仪表盘最新告警时间取值

--- src/views/rule-engine/Scene/Save/action/ListItem/FilterGroup.vue
+++ src/views/rule-engine/Scene/Save/action/ListItem/FilterGroup.vue
@@ -160,7 +160,7 @@ provide('filter-params', columnOptions);
 
 const columnRequest = () => {
     const param = {
-        branch: props.branchName,
+        branch: props.branchName === 0 ? props.branchName : props.branchName / 2,
         branchGroup: props.thenName,
         action: props.actionName,
     };
