292da350be5e010bf9994a0c9f4ea35b2e2c0f63
haodd <1903289197@qq.com>
2024-12-09 16:48:35 +0800

    feat: 新增子模块国际化文件引入

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -3,6 +3,18 @@ import { createI18n } from 'vue-i18n'
 import zhCn from './lang/zh.json'
 import enUS from './lang/en.json'
 
+const zhFiles = import.meta.glob('../modules/*/locales/lang/zh.json', {eager: true})
+const enFiles = import.meta.glob('../modules/*/locales/lang/en.json', {eager: true})
+
+Object.keys(zhFiles).forEach((key) => {
+    const result = (zhFiles[key] as any)?.default
+    Object.assign(zhCn, result)
+})
+
+Object.keys(enFiles).forEach((key) => {
+  const result = (zhFiles[key] as any)?.default
+  Object.assign(enUS, result)
+})
 
 const messages = {
   zh: zhCn,
