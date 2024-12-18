0db8c606bae122b5283aebcab49a6ec13215c3b6
haodd <1903289197@qq.com>
2024-12-18 16:30:22 +0800

    fix: 协议上传报错国际化未引入，文件上传地址拼接错误

--- src/api/comm.ts
+++ src/api/comm.ts
@@ -2,7 +2,7 @@ import { request } from '@jetlinks-web/core'
 import {getToken} from "@jetlinks-web/utils";
 import {BASE_API, TOKEN_KEY_URL} from '@jetlinks-web/constants';
 
-export const FileStatic = '/file/upload'
+export const FileStatic = 'file/upload'
 export const FileStaticPath = `${BASE_API}/${FileStatic}`
 export const getFileUrlById = (id: string) => `${BASE_API}/file/${id}?${TOKEN_KEY_URL}=${getToken()}`
 export const fileUpload = (data: any) => request.post(FileStatic, data)
