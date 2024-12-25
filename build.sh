57cc62ff4390e418dca52c03af07946b34d3bd71
XieYongHong <18010623010@163.com>
2024-12-25 18:08:18 +0800

    fix: 修改docker配置

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 docker build -t registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-vue:2.3.0-TEST .
+# docker build -t registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-vue:2.3.0-TEST --no-cache .
 docker push registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-vue:2.3.0-TEST
