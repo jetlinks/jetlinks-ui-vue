4aec16bde924996d7d0e96a5c5150dd45f386735
XieYongHong <18010623010@163.com>
2025-01-06 11:53:51 +0800

    fix: 优化国际化大小写

--- src/views/account/center/data.ts
+++ src/views/account/center/data.ts
@@ -1,14 +1,14 @@
 import i18n from "@/locales";
 type KeyType = 'HomeView' | 'BindThirdAccount' | 'Subscribe' | 'StationMessage';
 export const tabList: { key: KeyType; title: string }[] =  [
-    // {
-    //   key: 'HomeView',
-    //   title: '首页视图',
-    // },
-    // {
-    //   key: 'BindThirdAccount',
-    //   title: '绑定第三方账号',
-    // },
+    {
+      key: 'HomeView',
+      title: '首页视图',
+    },
+    {
+      key: 'BindThirdAccount',
+      title: '绑定第三方账号',
+    },
     {
         key: 'Subscribe',
         title: i18n.global.t('center.data.756829-0'),
