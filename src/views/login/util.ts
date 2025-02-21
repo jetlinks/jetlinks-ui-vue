34c7335e50a07073aeed1f0735adf5e170428d16
孙超 <54933585+sun-chaochao@users.noreply.github.com>
2025-02-21 09:31:54 +0800

    fix: 图片引入修改
    * fix: 图片引入修改

--- src/views/login/util.ts
+++ src/views/login/util.ts
@@ -1,6 +1,6 @@
 import standalone from '@/assets/internal-standalone.png'
 import dingtalk from '@/assets/dingtalk.png'
-import wechat from '@/assets/wechat.png'
+import wechat from '@/assets/wechat-webapp.png'
 import thirdParty from '@/assets/third-party.png'
 import wechatMiniapp from '@/assets/wechat-miniapp.png'
 
@@ -46,3 +46,4 @@ iconMap.set('wechat-webapp', wechat);
 iconMap.set('internal-standalone', standalone);
 iconMap.set('third-party', thirdParty);
 iconMap.set('wechat-miniapp', wechatMiniapp);
+
