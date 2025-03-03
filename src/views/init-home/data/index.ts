4486712e3dc2780460f87dd461867819fcd5c318
XieYongHong <18010623010@163.com>
2025-03-03 18:17:45 +0800

    fix: 优化初始化路由获取方式

--- src/views/init-home/data/index.ts
+++ src/views/init-home/data/index.ts
@@ -2,7 +2,7 @@ import proMenu from './baseMenu';
 
 const menusModule = import.meta.glob('../../../modules/**/baseMenu.ts', {eager: true});
 const modules = Object.values(menusModule);
-const menus = modules.map((item) => item.default);
+const menus = modules.map((item) => item.default?.());
 
 const baseModuleMenu = [...proMenu];
 
