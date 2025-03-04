3226e1db91470ecb60c48c920d84bbbe6b55a749
sunchao <2642441182@qq.com>
2025-03-04 18:23:28 +0800

    fix: merge

--- src/components/FormItem/Position.vue
+++ src/components/FormItem/Position.vue
@@ -31,7 +31,6 @@ const myValue = ref()
 const clickAddItem = () => {
   const tab = window.open(`${origin}/#/system/positions?save=true`);
   tab.onTabSaveSuccess = (value) => {
-
     myValue.value = props.extraProps?.multiple ? [...myValue.value, value] : value;
     emit('update:value', myValue.value);
     reload()
