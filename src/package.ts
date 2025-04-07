58c07f587bb73da1bbd4940409dc3a375d5fb92a
sunchao <2642441182@qq.com>
2025-04-07 17:04:06 +0800

    fix: 重定向登录修改

--- src/package.ts
+++ src/package.ts
@@ -41,6 +41,7 @@ const _handleReconnect = async () => {
     const modalWrapper = document.createElement('div');
     const modalApp = createApp(Relogin).mount(modalWrapper);
     document.body.appendChild(modalWrapper);
+
     return await modalApp?.open?.();
 }
 export const initAxios = () => {
