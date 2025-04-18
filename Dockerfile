5d03a83ca746f2583eea044810498b711bc99571
XieYongHong <18010623010@163.com>
2025-04-18 17:32:16 +0800

    fix: 修改docker配置，优化nginx配置

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM nginx
+FROM nginx:1.20.2
 ADD nginx.conf /etc/nginx/conf.d/default.conf
 ADD docker-entrypoint.sh /docker-entrypoint.sh
 COPY dist /usr/share/nginx/html
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 API_BASE_PATH=$API_BASE_PATH;
+SERVER_NAME=$SERVER_NAME;
+
 NAMESERVERS=$(cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' ')
 if [ -z "$API_BASE_PATH" ]; then
     API_BASE_PATH="http://jetlinks:8844/";
@@ -10,6 +12,19 @@ apiUrl="proxy_pass  $API_BASE_PATH\$1;"
 resolver="resolver $NAMESERVERS ipv6=off;"
 
 sed -i '11c '"$resolver"'' /etc/nginx/conf.d/default.conf
-sed -i '20c '"$apiUrl"'' /etc/nginx/conf.d/default.conf
+sed -i "s%{API_BASE_PATH}%$API_BASE_PATH%g" /etc/nginx/conf.d/default.conf
+
+if [ -z "$SERVER_NAME" ]; then
+    serverName="server_name localhost;"
+    sed -i '4c '"$serverName"'' /etc/nginx/conf.d/default.conf
+    sed -i "s%{SERVER_NAME}%\"0\"%g" /etc/nginx/conf.d/default.conf
+else
+    serverName="server_name $SERVER_NAME;"
+    sed -i '4c '"$serverName"'' /etc/nginx/conf.d/default.conf
+    sed -i "s%{SERVER_NAME}%$SERVER_NAME%g" /etc/nginx/conf.d/default.conf
+fi
+
+#cat /etc/nginx/conf.d/default.conf
 
 nginx -g "daemon off;"
+
