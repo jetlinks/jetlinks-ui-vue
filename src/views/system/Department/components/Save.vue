e4520ada225c4444a074c0085a02269c9cf22239
sunchao <2642441182@qq.com>
2025-04-29 16:39:39 +0800

    feat: 修改组织管理编辑修改

--- src/views/system/Department/components/Save.vue
+++ src/views/system/Department/components/Save.vue
@@ -1,32 +1,32 @@
 <template>
   <a-modal
-    visible
-    :title="title"
-    :maskClosable="false"
-    width="520px"
-    @cancel="emits('close')"
-    @ok="confirm"
-    class="edit-dialog-container"
-    :confirmLoading="loading"
+      visible
+      :title="title"
+      :maskClosable="false"
+      width="520px"
+      @cancel="emits('close')"
+      @ok="confirm"
+      class="edit-dialog-container"
+      :confirmLoading="loading"
   >
     <a-form ref="formRef" :model="formModel.data" layout="vertical">
       <a-form-item name="parentId" :label="$t('components.Save.069696-0')">
         <a-tree-select
-          v-model:value="formModel.data.parentId"
-          style="width: 100%"
-          :allow-clear="true"
-          :placeholder="$t('components.Save.069696-1')"
-          :tree-data="treeData"
-          :field-names="{ value: 'id' }"
-          @change="handleTreeSelectChange"
+            v-model:value="formModel.data.parentId"
+            style="width: 100%"
+            :allow-clear="true"
+            :placeholder="$t('components.Save.069696-1')"
+            :tree-data="treeData"
+            :field-names="{ value: 'id' }"
+            @change="handleTreeSelectChange"
         >
           <template #title="{ name }"> {{ name }} </template>
         </a-tree-select>
       </a-form-item>
       <a-form-item
-        name="name"
-        :label="$t('components.Save.069696-2')"
-        :rules="[
+          name="name"
+          :label="$t('components.Save.069696-2')"
+          :rules="[
           { required: true, message: $t('components.Save.069696-3') },
           { max: 64, message: $t('components.Save.069696-4') },
         ]"
@@ -35,15 +35,15 @@
       </a-form-item>
 
       <a-form-item
-        name="sortIndex"
-        :label="$t('components.Save.069696-5')"
-        :rules="[{ required: true, message: $t('components.Save.069696-6') }]"
+          name="sortIndex"
+          :label="$t('components.Save.069696-5')"
+          :rules="[{ required: true, message: $t('components.Save.069696-6') }]"
       >
         <a-input
-          v-model:value="formModel.data.sortIndex"
-          :placeholder="$t('components.Save.069696-6')"
-          :maxlength="64"
-          @blur="checkSort"
+            v-model:value="formModel.data.sortIndex"
+            :placeholder="$t('components.Save.069696-6')"
+            :maxlength="64"
+            @blur="checkSort"
         />
       </a-form-item>
     </a-form>
@@ -82,27 +82,27 @@ const formModel = reactive({
 
 // 保存数据
 const { loading, run } = useRequest(
-  props.data.id ? updateDepartment_api : addDepartment_api,
-  {
-    immediate: false,
-    onSuccess(res) {
-      if (res.success) {
-        onlyMessage($t('components.Save.069696-7'))
-        emits('save', res.result.id)
-      }
+    props.data.id ? updateDepartment_api : addDepartment_api,
+    {
+      immediate: false,
+      onSuccess(res) {
+        if (res.success) {
+          onlyMessage($t('components.Save.069696-7'))
+          emits('save', res.result.id)
+        }
+      },
     },
-  },
 )
 // 确认保存
 const confirm = () => {
   formRef.value
-    ?.validate()
-    .then((_data: any) => {
-      // if(!_data.parentId){
-      //   _data.parentId = undefined
-      // }
-      run(props.data.id ? { ...props.data, ..._data } : _data)
-    })
+      ?.validate()
+      .then((_data: any) => {
+        if(props.data.parentId && !_data.parentId){
+          _data.parentId = ''
+        }
+        run(props.data.id ? { ...props.data, ..._data } : _data)
+      })
 }
 
 // 顺序选择
@@ -126,13 +126,13 @@ const checkSort = (e: any) => {
 /**
  * 上级组织选择改变
  */
- const handleTreeSelectChange = () => {
+const handleTreeSelectChange = () => {
   // 上级组织
   const parent: any = treeData.value.find((f: any) => f.id === formModel.data.parentId)
   // 当前编辑的组织排序, 为选择上级组织的最大排序+1, 如上级组织没有自组织, 则默认为1
   formModel.data.sortIndex = parent?.children
-    ? parent.children[parent.children.length - 1].sortIndex + 1
-    : 1
+      ? parent.children[parent.children.length - 1].sortIndex + 1
+      : 1
 }
 
 //初始化
@@ -158,12 +158,12 @@ const init = () => {
 }
 
 watch(
-  () => props.data.id,
-  () => {
-    init()
-  },
-  {
-    immediate: true,
-  },
+    () => props.data.id,
+    () => {
+      init()
+    },
+    {
+      immediate: true,
+    },
 )
 </script>
