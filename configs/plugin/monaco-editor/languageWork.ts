94dddb64d398394f24bf4e1eee7409452294b561
XieYongHong <18010623010@163.com>
2025-06-05 14:12:56 +0800

    feat: 【monaco-editor】兼容特定环境下的微前端

---
+++ configs/plugin/monaco-editor/languageWork.ts
@@ -0,0 +1,36 @@
+export interface IWorkerDefinition {
+  label: string;
+  entry: string;
+}
+
+export const languageWorkAttr: IWorkerDefinition[] = [
+  {
+    label: 'editorWorkerService',
+    entry: 'monaco-editor/esm/vs/editor/editor.worker',
+  },
+  {
+    label: 'css',
+    entry: 'monaco-editor/esm/vs/language/css/css.worker',
+  },
+  {
+    label: 'html',
+    entry: 'monaco-editor/esm/vs/language/html/html.worker',
+  },
+  {
+    label: 'json',
+    entry: 'monaco-editor/esm/vs/language/json/json.worker',
+  },
+  {
+    label: 'typescript',
+    entry: 'monaco-editor/esm/vs/language/typescript/ts.worker',
+  },
+];
+
+
+export const languageWorksByLabel: { [language: string]: IWorkerDefinition } = {};
+languageWorkAttr.forEach(
+  (languageWork) => (languageWorksByLabel[languageWork.label] = languageWork)
+);
+
+
+export type EditorLanguageWorks = 'css' | 'html' | 'json' | 'typescript' | 'editorWorkerService'
