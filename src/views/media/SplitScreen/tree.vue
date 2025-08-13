4c19f74020bbb2e4837aee7d2ef5417fbda2393e
haodd0216 <1903289197@qq.com>
2025-08-13 16:36:39 +0800

    fix: 修复组织管理-左侧树数据量过大时页面卡死的问题；修复视频-分屏展示查询不出通道的bug(#35768、#35821)

--- src/views/media/SplitScreen/tree.vue
+++ src/views/media/SplitScreen/tree.vue
@@ -201,7 +201,7 @@ const getChildren = (node: any): Promise<any> => {
       terms: [
         {
           column: "parentId",
-          value: node._type === 'catalog' ? node.id : ''
+          value: node._type === 'catalog' ? node.id : node.deviceId
         }
       ]
     })
