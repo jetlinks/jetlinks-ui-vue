17ce2f28631afe3ba631c9727959b001189f77a5
XieYongHong <18010623010@163.com>
2025-09-18 15:46:52 +0800

    fix: 优化修改Docker镜像tag

--- build-template.sh
+++ build-template.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker build -f Dockerfile.template -t registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-template-ui:1.0 .
-docker push registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-template-ui:1.0
+docker build -f Dockerfile.template -t registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-template-ui:2.11.0 .
+docker push registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-template-ui:2.11.0
