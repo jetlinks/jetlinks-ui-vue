b04741b3519bb122985e01373a8077cd2c55caa7
XieYongHong <18010623010@163.com>
2025-09-28 15:24:05 +0800

    update: 添加图片压缩；优化Image使用和样式

--- vite.config.ts
+++ vite.config.ts
@@ -13,13 +13,14 @@ import convertLegacyToken from 'ant-design-vue/lib/theme/convertLegacyToken'
 import {
   registerModulesAlias,
   copyFile,
-  copyImagesPlugin,
 } from './configs/plugin'
-import { federation } from '@jetlinks-web/vite'
+import { federation, sharpOptimize } from '@jetlinks-web/vite'
+import customTheme from './configs/theme'
+import { antdLegacyVarsPlugin } from './configs/plugin/antd-legacy-vars-plugin'
 
 const {defaultAlgorithm, defaultSeed} = theme;
 
-const mapToken = defaultAlgorithm(defaultSeed);
+const mapToken = defaultAlgorithm({...defaultSeed, ...customTheme});
 const v3Token = convertLegacyToken(mapToken);
 
 const federationSharedMap = {
@@ -86,6 +87,7 @@ export default defineConfig(({mode}) => {
       vue(),
       vueJsx(),
       VueSetupExtend(),
+      antdLegacyVarsPlugin(),
       monacoEditorPlugin({
         languageWorkers: ['editorWorkerService', 'json', 'typescript']
       }),
@@ -102,7 +104,7 @@ export default defineConfig(({mode}) => {
       }),
       progress(),
       copyFile(mavenName),
-      copyImagesPlugin(),
+      // copyImagesPlugin(),
       federation({
         name: mavenName ? `${mavenName}` : 'host',
         remotes: {},
@@ -113,7 +115,8 @@ export default defineConfig(({mode}) => {
         exposes: mavenName ? {
           [mavenName] : `src/modules/${mavenName}/register.ts`
         } : undefined,
-      })
+      }),
+      sharpOptimize()
     ],
     server: {
       host: '0.0.0.0',
