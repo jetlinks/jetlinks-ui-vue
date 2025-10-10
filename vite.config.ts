f35363b5f276f177da67adf9e1c8ec503eaed46c
holt230 <lunghi.lin@qq.com>
2025-10-10 17:47:00 +0800

    feat: 新增子模块插件解析

--- vite.config.ts
+++ vite.config.ts
@@ -1,60 +1,43 @@
-import {defineConfig, loadEnv} from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
-import {VueAmapResolver} from '@vuemap/unplugin-resolver'
+import { VueAmapResolver } from '@vuemap/unplugin-resolver'
 import VueSetupExtend from 'vite-plugin-vue-setup-extend'
 import monacoEditorPlugin from './configs/plugin/monaco-editor'
 import progress from 'vite-plugin-progress'
 import * as path from 'path'
-import {theme} from 'ant-design-vue/lib'
+import { theme } from 'ant-design-vue/lib'
 import convertLegacyToken from 'ant-design-vue/lib/theme/convertLegacyToken'
-import {
-  registerModulesAlias,
-  copyFile,
-  excludeModulesPlugin,
-} from './configs/plugin'
+import { registerModulesAlias, copyFile, loadViteModulesPlugins } from './configs/plugin'
 import { federation, sharpOptimize } from '@jetlinks-web/vite'
 import customTheme from './configs/theme'
 import { antdLegacyVarsPlugin } from './configs/plugin/antd-legacy-vars-plugin'
 
-const {defaultAlgorithm, defaultSeed} = theme;
+const { defaultAlgorithm, defaultSeed } = theme
 
-const mapToken = defaultAlgorithm({...defaultSeed, ...customTheme});
-const v3Token = convertLegacyToken(mapToken);
+const mapToken = defaultAlgorithm({ ...defaultSeed, ...customTheme })
+const v3Token = convertLegacyToken(mapToken)
 
 const federationSharedMap = {
-  'vue': ['vue'],
+  vue: ['vue'],
   'vue-router': ['vue-router'],
-  'pinia': ['pinia'],
+  pinia: ['pinia'],
   'vue-i18n': ['vue-i18n'],
   'lodash-es': ['lodash-es'],
-  'echarts': ['echarts'],
+  echarts: ['echarts'],
   '@jetlinks-web/core': ['@jetlinks-web/core'],
   '@jetlinks-web/hooks': ['@jetlinks-web/hooks'],
-  '@jetlinks-web/constants': ['@jetlinks-web/constants'],
+  '@jetlinks-web/constants': ['@jetlinks-web/constants']
   // '@jetlinks-web/utils': ['@jetlinks-web/utils'],
 }
 // https://vitejs.dev/config/
-export default defineConfig(({mode}) => {
-
+export default defineConfig(({ mode }) => {
   const env: Partial<ImportMetaEnv> = loadEnv(mode, process.cwd())
 
-  const moduleNameIndex = process.argv.indexOf('--module-name');
-  const mavenName = moduleNameIndex !== -1 ? process.argv[moduleNameIndex + 1] : null;
-
-  // 解析 --omit 和 --pick 参数
-  const omitIndex = process.argv.indexOf('--omit');
-  const pickIndex = process.argv.indexOf('--pick');
-
-  const omitModules = omitIndex !== -1 && process.argv[omitIndex + 1]
-    ? process.argv[omitIndex + 1].split(',').filter(Boolean)
-    : [];
-
-  const pickModules = pickIndex !== -1 && process.argv[pickIndex + 1]
-    ? process.argv[pickIndex + 1].split(',').filter(Boolean)
-    : [];
+  const moduleNameIndex = process.argv.indexOf('--module-name')
+  const mavenName = moduleNameIndex !== -1 ? process.argv[moduleNameIndex + 1] : null
 
   return {
     base: './',
@@ -62,7 +45,7 @@ export default defineConfig(({mode}) => {
       alias: {
         '@': path.resolve(__dirname, 'src'),
         ...registerModulesAlias()
-      },
+      }
     },
     define: {
       'import.meta.env.VITE_MODULE_NAME': JSON.stringify(mavenName)
@@ -89,18 +72,14 @@ export default defineConfig(({mode}) => {
           },
           // 如果是模块构建，提取特定的CSS chunks
           ...(mavenName && {
-            input: `src/modules/${mavenName}/register.ts`,
+            input: `src/modules/${mavenName}/register.ts`
           }),
           compact: true,
-          manualChunks: mavenName ? undefined : federationSharedMap,
-        },
-      },
+          manualChunks: mavenName ? undefined : federationSharedMap
+        }
+      }
     },
     plugins: [
-      excludeModulesPlugin({
-        omit: omitModules,
-        pick: pickModules
-      }),
       vue(),
       vueJsx(),
       VueSetupExtend(),
@@ -109,29 +88,30 @@ export default defineConfig(({mode}) => {
         languageWorkers: ['editorWorkerService', 'json', 'typescript']
       }),
       Components({
-        resolvers: [
-          VueAmapResolver(),
-        ],
-        directoryAsNamespace: true,
+        resolvers: [VueAmapResolver()],
+        directoryAsNamespace: true
       }),
       AutoImport({
         imports: ['vue', 'vue-router'],
         dts: 'src/auto-imports.d.ts',
-        resolvers: [VueAmapResolver()],
+        resolvers: [VueAmapResolver()]
       }),
       progress(),
       copyFile(mavenName),
       // copyImagesPlugin(),
+      ...loadViteModulesPlugins(),
       federation({
         name: mavenName ? `${mavenName}` : 'host',
         remotes: {},
         enableDynamicRemotes: true,
         filename: mavenName ? 'remoteEntry.js' : undefined,
-        isHost:true,
+        isHost: true,
         shared: Object.keys(federationSharedMap),
-        exposes: mavenName ? {
-          [mavenName] : `src/modules/${mavenName}/register.ts`
-        } : undefined,
+        exposes: mavenName
+          ? {
+              [mavenName]: `src/modules/${mavenName}/register.ts`
+            }
+          : undefined
       }),
       sharpOptimize()
     ],
@@ -143,7 +123,7 @@ export default defineConfig(({mode}) => {
           target: env.VITE_APP_DEV_PROXY_URL,
           ws: true,
           changeOrigin: true,
-          rewrite: (path) => path.replace(new RegExp(`^${env.VITE_APP_BASE_API}`), ''),
+          rewrite: (path) => path.replace(new RegExp(`^${env.VITE_APP_BASE_API}`), '')
         }
       }
     },
@@ -152,26 +132,15 @@ export default defineConfig(({mode}) => {
         less: {
           modifyVars: {
             'root-entry-name': 'variable',
-            hack: `true; @import (reference) "${path.resolve(
-              'src/style/variable.less',
-            )}";`,
-            ...v3Token,
-
+            hack: `true; @import (reference) "${path.resolve('src/style/variable.less')}";`,
+            ...v3Token
           },
-          javascriptEnabled: true,
-        },
-      },
+          javascriptEnabled: true
+        }
+      }
     },
     optimizeDeps: {
-      include: [
-        'pinia',
-        'vue-router',
-        'axios',
-        'lodash-es',
-        '@vueuse/core',
-        'echarts',
-        'dayjs',
-      ],
-    },
+      include: ['pinia', 'vue-router', 'axios', 'lodash-es', '@vueuse/core', 'echarts', 'dayjs']
+    }
   }
 })
