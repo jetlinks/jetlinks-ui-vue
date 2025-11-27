c52d0002d6c06ef0eb40917e9b88dfd704a8fdb4
holt230 <lunghi.lin@qq.com>
2025-11-27 11:26:04 +0800

    refactor: improve tab instance handling and code formatting

--- src/hooks/useTabSaveSuccess.ts
+++ src/hooks/useTabSaveSuccess.ts
@@ -14,18 +14,21 @@ type OptionsType = {
 export const useTabSaveSuccess = (code: string = '', options?: OptionsType) => {
   const id = ref('tab-save-success' + randomString(8))
   const menuStore = useMenuStore()
-  let tabInstance: WindowProxy
+  const tabInstance = ref<WindowProxy | null>(null)
 
   function formatPath(str: string, obj: Record<string, string>) {
     return str.replace(/:([a-zA-Z_]\w*)/g, (_, key) => {
-      return obj.hasOwnProperty(key) ? obj[key] : '';
-    });
+      return obj.hasOwnProperty(key) ? obj[key] : ''
+    })
   }
 
-  const onOpen = (params: Record<string, any>, _options: {
-    menuCode?: string,
-    menuParams?: Record<string, any>
-  } = {}): Promise<any> => {
+  const onOpen = (
+    params: Record<string, any>,
+    _options: {
+      menuCode?: string
+      menuParams?: Record<string, any>
+    } = {}
+  ): Promise<any> => {
     return new Promise((resolve, reject) => {
       const menuItem = menuStore.getMenu(_options.menuCode || code)
 
@@ -34,30 +37,33 @@ export const useTabSaveSuccess = (code: string = '', options?: OptionsType) => {
         return
       }
 
-      const _params = new URLSearchParams({...params, sourceId: id.value})
+      const _params = new URLSearchParams({ ...params, sourceId: id.value })
       const hash = location.hash ? '/#' : ''
-      const path = _options.menuParams ? formatPath(menuItem.path, _options.menuParams)  : menuItem.path
+      const path = _options.menuParams ? formatPath(menuItem.path, _options.menuParams) : menuItem.path
       const url = [location.origin, hash, path, '?', _params.toString()].join('')
 
-      if (isSubApp) { // 微前端
-        const globalData = (window as any).microApp.getGlobalData() as { api: Record<string, any>}
+      if (isSubApp) {
+        // 微前端
+        const globalData = (window as any).microApp.getGlobalData() as { api: Record<string, any> }
         if (globalData?.api?.onTabSaveSuccess) {
           globalData.api.onTabSaveSuccess(id.value, url, options)
         }
       } else {
-        tabInstance = window.open(url) as WindowProxy
-        (tabInstance as any).onTabSaveSuccess = (_sourceId: string, value: any) => {
+        tabInstance.value = window.open(url) as WindowProxy
+        ;(tabInstance.value as any).onTabSaveSuccess = (_sourceId: string, value: any) => {
           if (_sourceId === id.value) {
             options?.onSuccess?.(value)
             resolve(value)
           }
         }
+        resolve(tabInstance.value)
       }
     })
   }
 
   return {
     onOpen,
+    tabInstance
   }
 }
 
@@ -71,12 +77,12 @@ export const useTabSaveSuccessBack = () => {
       const isClose = options?.onBefore ? await options.onBefore() : true
       if (isClose) {
         if (isSubApp) {
-          globalData = (window as any).microApp.getGlobalData() as { api: Record<string, any>}
+          globalData = (window as any).microApp.getGlobalData() as { api: Record<string, any> }
           if (globalData?.api?.onTabSaveSuccessBack) {
             globalData.api.onTabSaveSuccessBack(sourceId, data)
           }
         } else if ((window as any).onTabSaveSuccess) {
-          (window as any).onTabSaveSuccess(sourceId, data)
+          ;(window as any).onTabSaveSuccess(sourceId, data)
           setTimeout(() => window.close(), 300)
         }
       }
